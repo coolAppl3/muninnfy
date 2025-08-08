1d258530a13cde377dd9493add2b44928a2d5424
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-08-08 22:17:24 +0200

    feat: add Button component

---
+++ client/src/components/Button/Button.tsx
@@ -0,0 +1,22 @@
+import { JSX, MouseEventHandler, ReactNode } from 'react';
+import './Button.css';
+
+interface ButtonProps {
+  content: ReactNode;
+  className?: string;
+  disabled?: boolean;
+  isSubmitBtn?: boolean;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function Button({ content, className = '', disabled = false, isSubmitBtn = false, onClick }: ButtonProps): JSX.Element {
+  return (
+    <button
+      type={isSubmitBtn ? 'submit' : 'button'}
+      className={`Button ${className} ${disabled ? 'disabled' : ''}`}
+      onClick={onClick}
+    >
+      {content}
+    </button>
+  );
+}
