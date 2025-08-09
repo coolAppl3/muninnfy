04b1ea2663ec6127cc4985e8ec2427673b7a72e6
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-08-09 23:06:25 +0200

    build: include svgr plugin

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      svgrOptions: {
+        replaceAttrValues: {
+          '#D9D9D9': 'currentColor',
+        },
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
