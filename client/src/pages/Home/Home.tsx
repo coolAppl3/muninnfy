fcd44ed707822616c3aea6ff4ac835809d800dfc
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-08-11 01:43:46 +0200

    feat: add Hero component for Home page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import { JSX } from 'react';
 import { Head } from '../../components/Head/Head';
+import Hero from './Hero';
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Head title='Home - Muninnfy' />
+      <Hero />
     </>
   );
 }
