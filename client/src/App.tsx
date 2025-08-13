5d1eb76da8747965a4760263ef241bb3171f7f88
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-08-13 21:16:03 +0200

    feat: add Footer component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import { JSX } from 'react';
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Navbars from './components/Navbars/Navbars';
+import Footer from './components/Footer/Footer';
 
 export default function App(): JSX.Element {
   return (
     <>
       <Navbars />
       <Outlet />
+      <Footer />
     </>
   );
 }
