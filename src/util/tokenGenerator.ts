34c7bad86ec3ea69876c54905e1fb087b99a43ab
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-08-14 00:25:44 +0200

    refactor: move isValidVerificationCode() to tokenGenerator, and align with the number 0 being excluded

--- src/util/tokenGenerator.ts
+++ src/util/tokenGenerator.ts
@@ -2,6 +2,15 @@ export function generateCryptoUuid(): string {
   return crypto.randomUUID();
 }
 
+export function isValidUuid(uuid: string): boolean {
+  if (uuid.length !== 36) {
+    return false;
+  }
+
+  const regex: RegExp = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  return regex.test(uuid);
+}
+
 export function generateVerificationCode(): string {
   // number 0, as well as uppercase and lowercase O, not included
   const allowedCodeCharacters: string = 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789';
@@ -12,4 +21,13 @@ export function generateVerificationCode(): string {
   }
 
   return verificationCode;
+}
+
+export function isValidVerificationCode(verificationCode: any): boolean {
+  if (typeof verificationCode !== 'string') {
+    return false;
+  }
+
+  const regex: RegExp = /^[A-NP-Z1-9]{6}$/;
+  return regex.test(verificationCode);
 }
