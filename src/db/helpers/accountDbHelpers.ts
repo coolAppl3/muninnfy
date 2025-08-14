f71d1ccf850d07da7ad63b047a4ce6bed4e40d50
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-08-14 23:37:11 +0200

    feat: add incrementVerificationEmailsSent() helper

--- src/db/helpers/accountDbHelpers.ts
+++ src/db/helpers/accountDbHelpers.ts
@@ -17,6 +17,25 @@ export async function deleteAccountById(accountId: number, executor: Pool | Pool
   }
 }
 
+export async function incrementVerificationEmailsSent(verificationId: number, executor: Pool | PoolConnection): Promise<boolean> {
+  try {
+    const [resultSetHeader] = await executor.execute<ResultSetHeader>(
+      `UPDATE
+        account_verification
+      SET
+        verification_emails_sent = verification_emails_sent + 1
+      WHERE
+        verification_id = ?;`,
+      [verificationId]
+    );
+
+    return resultSetHeader.affectedRows > 0;
+  } catch (err: unknown) {
+    console.log(err);
+    return false;
+  }
+}
+
 export async function incrementFailedVerificationAttempts(verificationId: number, executor: Pool | PoolConnection): Promise<boolean> {
   try {
     const [resultSetHeader] = await executor.execute<ResultSetHeader>(
