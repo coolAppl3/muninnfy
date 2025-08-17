89d4f2d9fbbbf6a1e284e9b6b0c4d6c73fa53746
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-08-17 13:48:39 +0200

    feat: add useEffect hook to reset scroll position when the pathname changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
-import { JSX } from 'react';
-import { Outlet } from 'react-router-dom';
+import { JSX, useEffect } from 'react';
+import { Location, Outlet, useLocation } from 'react-router-dom';
 import './App.css';
 import Navbars from './components/Navbars/Navbars';
 import Footer from './components/Footer/Footer';
 
 export default function App(): JSX.Element {
+  const { pathname }: Location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <Navbars />
