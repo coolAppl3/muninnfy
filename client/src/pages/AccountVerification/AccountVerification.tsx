904a1c2e82c1f48ebe300628ec9cbb6588b37786
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-08-23 01:44:45 +0200

    feat: implement ResendAccountVerificationEmail local component

--- client/src/pages/AccountVerification/AccountVerification.tsx
+++ client/src/pages/AccountVerification/AccountVerification.tsx
@@ -1,7 +1,12 @@
 import { JSX } from 'react';
 import { Head } from '../../components/Head/Head';
 import Container from '../../components/Container/Container';
-import { useSearchParams } from 'react-router-dom';
+import { NavigateFunction, useNavigate, useSearchParams } from 'react-router-dom';
+import Button from '../../components/Button/Button';
+import { resendAccountVerificationEmailService } from '../../services/accountServices';
+import usePopupMessage from '../../hooks/usePopupMessage';
+import { AsyncErrorData, getAsyncErrorData } from '../../utils/errorUtils';
+import useInfoModal from '../../hooks/useInfoModal';
 
 export default function AccountVerification(): JSX.Element {
   const [searchParams] = useSearchParams();
@@ -16,28 +21,97 @@ export default function AccountVerification(): JSX.Element {
       <Head title='Account Verification - Muninnfy' />
 
       <section className='py-4 h-available flex justify-center items-center'>
-        <Container>{renderedJsx}</Container>
+        <Container>
+          <div className='py-3 px-2 bg-secondary rounded-sm shadow-simple max-w-[36rem] mx-auto'>{renderedJsx}</div>
+        </Container>
       </section>
     </>
   );
 }
 
 function ContinueAccountVerificationForm(): JSX.Element {
-  return <div>{/* TODO: implement */}</div>;
+  return <>{/* TODO: implement */}</>;
 }
 
-function ResendAccountVerification(): JSX.Element {
-  return <div>{/* TODO: implement */}</div>;
+function ResendAccountVerificationEmail({ publicAccountId }: { publicAccountId: string }): JSX.Element {
+  const navigate: NavigateFunction = useNavigate();
+
+  const { displayPopupMessage } = usePopupMessage();
+  const { displayInfoModal, removeInfoModal } = useInfoModal();
+
+  const infoModalErrorRecord: Record<number, { description: string | undefined; onClick?: () => void }> = {
+    400: {
+      description: 'Check your inbox for a verification email, or start the sign up process again.',
+      onClick: () => navigate('/account/verification'),
+    },
+
+    403: {
+      description: `Emails may a minute to reach you, and could end up in your spam folder.\nIf you still can't find the email, wait 20 minutes and start again.`,
+      onClick: undefined,
+    },
+
+    404: {
+      description: `Accounts are deleted within 20 minutes of being created if left unverified.\nYou can always start the sign up process again.`,
+      onClick: () => navigate('/sign-up'),
+    },
+
+    409: { description: 'You can simply proceed with singing in.', onClick: () => navigate('/sign-in') },
+  };
+
+  async function handleOnClick(): Promise<void> {
+    try {
+      await resendAccountVerificationEmailService({ publicAccountId });
+      displayPopupMessage('Email resent.', 'success');
+    } catch (err: unknown) {
+      console.log(err);
+      const asyncErrorData: AsyncErrorData | null = getAsyncErrorData(err);
+
+      if (!asyncErrorData) {
+        displayPopupMessage('Something went wrong.', 'error');
+        return;
+      }
+
+      const { status, errMessage, errReason } = asyncErrorData;
+      displayPopupMessage(errMessage, 'error');
+
+      if (!errReason || ![400, 403, 404, 409].includes(status)) {
+        return;
+      }
+
+      const description: string | undefined = infoModalErrorRecord[403]?.description;
+      const onClick: (() => void) | undefined = infoModalErrorRecord[403]?.onClick;
+
+      displayInfoModal({
+        title: errMessage,
+        description,
+        btnTitle: 'Okay',
+        onClick: onClick || removeInfoModal,
+      });
+    }
+  }
+
+  return (
+    <>
+      <h4 className='text-title font-medium mb-1'>Ongoing account verification detected.</h4>
+      <p className='text-description text-sm mb-2'>Check your inbox for a verification link, and click it to continue.</p>
+      <Button
+        className='bg-description border-description text-dark w-full'
+        onClick={handleOnClick}
+      >
+        Resend email
+      </Button>
+    </>
+  );
 }
 
 function ConfirmAccountVerification(): JSX.Element {
-  return <div>{/* TODO: implement */}</div>;
+  return <>{/* TODO: implement */}</>;
 }
 
 function determineRenderedJsx(publicAccountId: string | null, verificationToken: string | null): JSX.Element {
   if (publicAccountId) {
-    // verificationToken on its own is not useful
-    return verificationToken ? <ConfirmAccountVerification /> : <ResendAccountVerification />;
+    // verificationToken on its own is not useful and will be ignored
+    return verificationToken ? <ConfirmAccountVerification /> : <ResendAccountVerificationEmail publicAccountId={publicAccountId} />;
   }
 
   return <ContinueAccountVerificationForm />;
