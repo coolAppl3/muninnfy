8a3952f0a603deab2cb1910872e897a4324dccd9
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-08-22 02:53:22 +0200

    refactor: prevent modal persisting through navigation

--- client/src/providers/InfoModalProvider.tsx
+++ client/src/providers/InfoModalProvider.tsx
@@ -1,16 +1,22 @@
-import { JSX, ReactNode, useState } from 'react';
+import { JSX, ReactNode, useEffect, useState } from 'react';
 import InfoModalContext from '../contexts/InfoModalContext';
 import InfoModal, { InfoModalProps } from '../components/InfoModal/InfoModal';
+import { Location, useLocation } from 'react-router-dom';
 
 export default function InfoModalProvider({ children }: { children: ReactNode }): JSX.Element {
-  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const routerLocation: Location = useLocation();
 
+  const [isVisible, setIsVisible] = useState<boolean>(false);
   const [InfoModalState, setInfoModalState] = useState<InfoModalProps>({
     title: undefined,
     description: undefined,
     btnTitle: 'Okay',
     onClick: removeInfoModal,
   });
+
+  useEffect(() => {
+    removeInfoModal();
+  }, [routerLocation]);
 
   function displayInfoModal(props: InfoModalProps): void {
     setInfoModalState({ ...props });
@@ -19,6 +25,12 @@ export default function InfoModalProvider({ children }: { children: ReactNode })
 
   function removeInfoModal(): void {
     setIsVisible(false);
+    setInfoModalState({
+      title: undefined,
+      description: undefined,
+      btnTitle: 'Okay',
+      onClick: removeInfoModal,
+    });
   }
 
   const { title, description, btnTitle, onClick } = InfoModalState;
