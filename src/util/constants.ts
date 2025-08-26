1b4b4c3559b2350386bed9ab97550c3a7aaecfb3
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-08-26 22:30:19 +0200

    feat: add AUTH_EXTENSIONS_LIMIT constant

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -15,3 +15,4 @@ export const REQUESTS_RATE_LIMIT: number = 60;
 export const LIGHT_DAILY_RATE_ABUSE_COUNT: number = 10;
 
 export const AUTH_SESSIONS_LIMIT: number = 3;
+export const AUTH_EXTENSIONS_LIMIT: number = 3;
