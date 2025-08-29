b7d5f9952a124e75a3ada0279b5b59e6342fafff
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-08-30 00:03:21 +0200

    feat: add Head to Account

--- client/src/pages/Account/Account.tsx
+++ client/src/pages/Account/Account.tsx
@@ -1,5 +1,10 @@
 import { JSX } from 'react';
+import { Head } from '../../components/Head/Head';
 
 export default function Account(): JSX.Element {
-  return <div></div>;
+  return (
+    <>
+      <Head title='Account - Muninnfy' />
+    </>
+  );
 }
