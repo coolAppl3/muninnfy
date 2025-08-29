02c05dd5513015d4483b98ba1191b24f20248ed5
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-08-30 01:18:24 +0200

    feat: add wishlistValidation with a few validation functions

---
+++ src/util/validation/wishlistValidation.ts
@@ -0,0 +1,48 @@
+import { PRIVATE_WISHLIST_PRIVACY_LEVEL, PUBLIC_WISHLIST_PRIVACY_LEVEL } from '../constants';
+import { containsInvalidWhitespace } from '../globalUtils';
+
+export function isValidWishlistPrivacyLevel(privacyLevel: any): boolean {
+  if (typeof privacyLevel !== 'number') {
+    return false;
+  }
+
+  if (!Number.isInteger(privacyLevel)) {
+    return false;
+  }
+
+  if (privacyLevel < PRIVATE_WISHLIST_PRIVACY_LEVEL || privacyLevel > PUBLIC_WISHLIST_PRIVACY_LEVEL) {
+    return false;
+  }
+
+  return true;
+}
+
+export function isValidWishlistTitle(title: any): boolean {
+  if (typeof title !== 'string') {
+    return false;
+  }
+
+  if (containsInvalidWhitespace(title)) {
+    return false;
+  }
+
+  const regex: RegExp = /^(?=.{1,50}$)(?!.*  )[\x00-\x7F]+(?: [\x00-\x7F]+)*$/;
+  return regex.test(title);
+}
+
+export function isValidWishlistDescription(description: any): boolean {
+  if (description === null) {
+    return true;
+  }
+
+  if (typeof description !== 'string') {
+    return false;
+  }
+
+  if (containsInvalidWhitespace(description)) {
+    return false;
+  }
+
+  const regex: RegExp = /^(?=.{1,500}$)(?!.*  )[\x00-\x7F]+(?: [\x00-\x7F]+)*$/;
+  return regex.test(description);
+}
