97ce5792fcf1c25257820a7795d65dafea2fa3f2
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-08-30 02:10:41 +0200

    feat: add TOTAL_WISHLISTS_LIMIT

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -20,3 +20,5 @@ export const AUTH_EXTENSIONS_LIMIT: number = 3;
 export const PRIVATE_WISHLIST_PRIVACY_LEVEL: number = 0;
 export const FOLLOWERS_WISHLIST_PRIVACY_LEVEL: number = 1;
 export const PUBLIC_WISHLIST_PRIVACY_LEVEL: number = 2;
+
+export const TOTAL_WISHLISTS_LIMIT: number = 10_000;
