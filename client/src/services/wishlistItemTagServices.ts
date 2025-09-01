8d7f6549b15ad191a9bfb6ccbec785417ca26f88
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-09-02 01:31:48 +0200

    feat: add wishlistItemTagServices with addWishlistItemTagService()

---
+++ client/src/services/wishlistItemTagServices.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosResponse } from 'axios';
+
+axios.defaults.withCredentials = true;
+const wishlistItemTagsApiUrl: string =
+  location.hostname === 'localhost' ? `http://localhost:5000/api/wishlistItemTags` : `https://muninnfy/api/wishlistItemTags`;
+
+interface AddWishlistItemTagServicePayload {
+  wishlistId: string;
+  itemId: number;
+  tagName: string;
+}
+
+interface AddWishlistItemTagServiceData {
+  tagId: number;
+}
+
+export async function addWishlistItemTagService(
+  body: AddWishlistItemTagServicePayload
+): Promise<AxiosResponse<AddWishlistItemTagServiceData>> {
+  return axios.post(wishlistItemTagsApiUrl, body);
+}
