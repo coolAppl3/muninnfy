ab8e5a8719ed4efc8bfdbe4798d8e7f0f7161095
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-09-01 02:08:57 +0200

    feat: add wishlistItemValidation with a few validation functions

---
+++ src/util/validation/wishlistItemValidation.ts
@@ -0,0 +1,32 @@
+import { containsInvalidWhitespace } from '../globalUtils';
+
+export function isValidWishlistItemTitle(value: any): boolean {
+  if (typeof value !== 'string') {
+    return false;
+  }
+
+  if (containsInvalidWhitespace(value)) {
+    return false;
+  }
+
+  const regex: RegExp = /^(?=.{1,50}$)(?!.*  )[\x00-\x7F]+(?: [\x00-\x7F]+)*$/;
+  return regex.test(value);
+}
+
+export function isValidWishlistItemDescription(value: any): boolean {
+  if (typeof value !== 'string') {
+    return false;
+  }
+
+  const regex: RegExp = /^[\x00-\x7F]{1,500}$/;
+  return regex.test(value);
+}
+
+export function isValidWishlistItemLink(value: any): boolean {
+  if (typeof value !== 'string') {
+    return false;
+  }
+
+  const regex = /^(?=.{1,2000}$)(https?:\/\/[^\s/$.?#].[^\s]*)$/i;
+  return regex.test(value);
+}
