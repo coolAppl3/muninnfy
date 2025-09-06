b5e400ee072f2f074bbe686c420833b0479ac07c
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-09-07 01:21:37 +0200

    refactor: remove signOut function from AuthProvider

--- client/src/contexts/AuthContext.ts
+++ client/src/contexts/AuthContext.ts
@@ -3,7 +3,6 @@ import { createContext, Dispatch, SetStateAction } from 'react';
 export interface AuthContextInterface {
   isSignedIn: boolean;
   setIsSignedIn: Dispatch<SetStateAction<boolean>>;
-  signOut: () => Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextInterface | null>(null);
--- client/src/providers/AuthProvider.tsx
+++ client/src/providers/AuthProvider.tsx
@@ -1,14 +1,9 @@
-import { JSX, ReactNode, useCallback, useEffect, useMemo, useState } from 'react';
+import { JSX, ReactNode, useEffect, useMemo, useState } from 'react';
 import AuthContext, { AuthContextInterface } from '../contexts/AuthContext';
-import { checkForAuthSessionService, signOutService } from '../services/authServices';
-import useLoadingOverlay from '../hooks/useLoadingOverlay';
-import usePopupMessage from '../hooks/usePopupMessage';
+import { checkForAuthSessionService } from '../services/authServices';
 
 export default function AuthProvider({ children }: { children: ReactNode }): JSX.Element {
   const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
-
-  const { displayLoadingOverlay, removeLoadingOverlay } = useLoadingOverlay();
-  const { displayPopupMessage } = usePopupMessage();
 
   useEffect(() => {
     const abortController: AbortController = new AbortController();
@@ -29,22 +24,6 @@ export default function AuthProvider({ children }: { children: ReactNode }): JSX
     };
   }, []);
 
-  const signOut = useCallback(async (): Promise<void> => {
-    displayLoadingOverlay();
-
-    try {
-      await signOutService();
-      setIsSignedIn(false);
-
-      displayPopupMessage('Signed out.', 'success');
-    } catch (err: unknown) {
-      console.log(err);
-      displayPopupMessage('Sign out failed.', 'success');
-    } finally {
-      removeLoadingOverlay();
-    }
-  }, [displayLoadingOverlay, removeLoadingOverlay, displayPopupMessage]);
-
-  const contextValue: AuthContextInterface = useMemo(() => ({ isSignedIn, setIsSignedIn, signOut }), [isSignedIn, signOut]);
+  const contextValue: AuthContextInterface = useMemo(() => ({ isSignedIn, setIsSignedIn }), [isSignedIn]);
   return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>;
 }
