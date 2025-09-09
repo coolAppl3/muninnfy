abe2a09db2835b6b6ca1051ac5457fcf303c69a7
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-09-09 02:19:39 +0200

    feat: add LoadingSkeleton component

---
+++ client/src/components/LoadingSkeleton/LoadingSkeleton.tsx
@@ -0,0 +1,55 @@
+import { JSX } from 'react';
+import Container from '../Container/Container';
+import './LoadingSkeleton.css';
+
+export default function LoadingSkeleton(): JSX.Element {
+  return (
+    <div id='loading-skeleton'>
+      <Container>
+        <div className='grid gap-2'>
+          <div className='skeleton-container'>
+            <div className='text max-w-1/3 mb-2'></div>
+            <div className='text skinny mb-1'></div>
+            <div className='text skinny max-w-2/3'></div>
+          </div>
+
+          <div className='skeleton-container'>
+            <div className='text max-w-3/4 mb-2'></div>
+
+            <div className='text skinny mb-1'></div>
+            <div className='text skinny max-w-1/3 mb-2'></div>
+
+            <div className='text skinny mb-1'></div>
+            <div className='text skinny max-w-3/5 mb-2'></div>
+          </div>
+
+          <div className='skeleton-grid'>
+            <div className='skeleton-container'>
+              <div className='text skinny'></div>
+              <div className='text extra-skinny'></div>
+              <div className='text extra-skinny'></div>
+            </div>
+
+            <div className='skeleton-container'>
+              <div className='text skinny'></div>
+              <div className='text extra-skinny'></div>
+              <div className='text extra-skinny'></div>
+            </div>
+
+            <div className='skeleton-container'>
+              <div className='text skinny'></div>
+              <div className='text extra-skinny'></div>
+              <div className='text extra-skinny'></div>
+            </div>
+
+            <div className='skeleton-container'>
+              <div className='text skinny'></div>
+              <div className='text extra-skinny'></div>
+              <div className='text extra-skinny'></div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -14,6 +14,7 @@ import NotFound from '../pages/NotFound/NotFound';
 import { AuthStatus } from '../contexts/AuthContext';
 import useAuth from '../hooks/useAuth';
 import useHistory from '../hooks/useHistory';
+import LoadingSkeleton from '../components/LoadingSkeleton/LoadingSkeleton';
 
 interface RouteDetails {
   path: string;
@@ -106,7 +107,7 @@ function AuthOnlyRoute({ authStatus, redirectTo = '/sign-in' }: { authStatus: Au
   }, [authStatus, pathname, search, postAuthNavigate, setPostAuthNavigate]);
 
   if (authStatus === 'loading') {
-    return <>{/* TODO: implement loading skeleton */}</>;
+    return <LoadingSkeleton />;
   }
 
   if (authStatus === 'authenticated') {
@@ -125,7 +126,7 @@ function NonAuthOnlyRoute({ authStatus, redirectTo = '/account' }: { authStatus:
   const { postAuthNavigate } = useHistory();
 
   if (authStatus === 'loading') {
-    return <>{/* TODO: implement loading skeleton */}</>;
+    return <LoadingSkeleton />;
   }
 
   if (authStatus === 'unauthenticated') {
