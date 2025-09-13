bed27452f24eb30fa304f7642d29793a8be83e21
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-09-13 02:21:41 +0200

    feat: add wishlistItemValidation with a few validation functions

---
+++ client/src/utils/validation/wishlistItemValidation.ts
@@ -0,0 +1,50 @@
+export function validateWishlistItemTitle(value: string): string | null {
+  if (value === '') {
+    return 'A valid item title is required.';
+  }
+
+  if (value !== value.trim()) {
+    return 'Title must not contain leading or trailing whitespace.';
+  }
+
+  if (/\s{2,}/.test(value)) {
+    return 'Title must not contain consecutive whitespaces';
+  }
+
+  if (value.length > 50) {
+    return 'Title must not exceed 50 characters.';
+  }
+
+  const regex: RegExp = /^(?=.{1,50}$)(?!.*  )[\x00-\x7F]+(?: [\x00-\x7F]+)*$/;
+  if (!regex.test(value)) {
+    return 'Only standard letters, numbers, and symbols are allowed.';
+  }
+
+  return null;
+}
+
+export function validateWishlistItemDescription(value: string): string | null {
+  if (value !== value.trim()) {
+    return 'Description must not contain leading or trailing whitespace.';
+  }
+
+  if (value.length > 500) {
+    return 'Description must not exceed 500 characters.';
+  }
+
+  const regex: RegExp = /^[\x00-\x7F]{1,500}$/;
+  if (!regex.test(value)) {
+    return 'Only standard letters, numbers, and symbols are allowed.';
+  }
+
+  return null;
+}
+
+export function validateWishlistItemLink(value: string): string | null {
+  const regex: RegExp = /^(?=.{1,50}$)(?!.*  )[\x00-\x7F]+(?: [\x00-\x7F]+)*$/;
+  if (!regex.test(value)) {
+    return 'Invalid item link.';
+  }
+
+  return null;
+}
