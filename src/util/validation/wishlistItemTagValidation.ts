20ed9f6e5b44397cb93ca7b3e1899138e7408a7c
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-09-14 20:04:54 +0200

    feat: add sanitizedWishlistItemTags()

--- src/util/validation/wishlistItemTagValidation.ts
+++ src/util/validation/wishlistItemTagValidation.ts
@@ -1,3 +1,5 @@
+import { WISHLIST_ITEM_TAGS_LIMIT } from '../constants/wishlistItemConstants';
+
 export function isValidWishlistItemTagName(value: any): boolean {
   if (typeof value !== 'string') {
     return false;
@@ -5,4 +7,27 @@ export function isValidWishlistItemTagName(value: any): boolean {
 
   const regex: RegExp = /^[A-Za-z0-9_]{1,50}$/;
   return regex.test(value);
+}
+
+export function sanitizedWishlistItemTags(tags: any[], itemId: number): [number, string][] {
+  const sanitizedTags: [number, string][] = [];
+  const tagsSet = new Set<string>();
+
+  for (const tag of tags.slice(0, WISHLIST_ITEM_TAGS_LIMIT)) {
+    if (typeof tag !== 'string') {
+      continue;
+    }
+
+    if (!isValidWishlistItemTagName(tag)) {
+      continue;
+    }
+
+    tagsSet.add(tag);
+  }
+
+  for (const tag of tagsSet) {
+    sanitizedTags.push([itemId, tag]);
+  }
+
+  return sanitizedTags;
 }
