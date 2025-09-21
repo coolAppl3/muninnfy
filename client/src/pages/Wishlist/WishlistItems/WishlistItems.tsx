73d4286748dbc4f8be616aa8d73d013ea0b37a98
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-09-22 01:12:55 +0200

    feat: implement logic to handle rendering when a wishlist has no items

--- client/src/pages/Wishlist/WishlistItems/WishlistItems.tsx
+++ client/src/pages/Wishlist/WishlistItems/WishlistItems.tsx
@@ -5,18 +5,22 @@ import { WishlistItemInterface } from '../../../services/wishlistServices';
 import WishlistItem from './WishlistItem/WishlistItem';
 
 export default function WishlistItems(): JSX.Element {
-  const { wishlistItems, setWishlistItems } = useWishlist();
+  const { wishlistItems } = useWishlist();
 
   return (
     <section className='wishlist-items'>
       <Container>
         <div className='wishlist-items-container'>
-          {wishlistItems.map((item: WishlistItemInterface) => (
-            <WishlistItem
-              item={item}
-              key={item.item_id}
-            />
-          ))}
+          {wishlistItems.length === 0 ? (
+            <p className='no-items'>No items found</p>
+          ) : (
+            wishlistItems.map((item: WishlistItemInterface) => (
+              <WishlistItem
+                item={item}
+                key={item.item_id}
+              />
+            ))
+          )}
         </div>
       </Container>
     </section>
