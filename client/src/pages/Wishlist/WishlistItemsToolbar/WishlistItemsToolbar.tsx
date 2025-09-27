7228a5b197e08f1c506bd8185310e03a442b3cbd
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-09-27 02:45:29 +0200

    feat: add stub for WishlistItemsToolbarView component

--- client/src/pages/Wishlist/WishlistItemsToolbar/WishlistItemsToolbar.tsx
+++ client/src/pages/Wishlist/WishlistItemsToolbar/WishlistItemsToolbar.tsx
@@ -1,11 +1,10 @@
 import { JSX, useState } from 'react';
 import Container from '../../../components/Container/Container';
 import DefaultFormGroup from '../../../components/FormGroups/DefaultFormGroup';
-import SingleColumnGridIcon from '../../../assets/svg/SingleColumnGridIcon.svg?react';
-import DoubleColumnGridIcon from '../../../assets/svg/DoubleColumnGridIcon.svg?react';
 import SlidersIcon from '../../../assets/svg/SlidersIcon.svg?react';
 import WishlistItemsToolbarOptions from './components/WishlistItemsToolbarOptions';
 import WishlistItemsToolbarSort from './components/WishlistItemsToolbarSort';
+import WishlistItemsToolbarView from './components/WishlistItemsToolbarView';
 
 export default function WishlistItemsToolbar(): JSX.Element {
   const [value, setValue] = useState<string>('');
@@ -15,20 +14,7 @@ export default function WishlistItemsToolbar(): JSX.Element {
       <Container>
         <div className='text-title'>
           <div className='flex justify-start items-center gap-1 mb-1'>
-            <div className='view-container flex justify-center items-center mr-auto'>
-              <button
-                type='button'
-                className='selected toolbar-btn'
-              >
-                <DoubleColumnGridIcon />
-              </button>
-              <button
-                type='button'
-                className='toolbar-btn'
-              >
-                <SingleColumnGridIcon />
-              </button>
-            </div>
+            <WishlistItemsToolbarView />
 
             <button
               type='button'
