c80504ff4a6eb5f1f2d1fd3b30e1ad5a6b02cc95
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-09-30 03:10:17 +0200

    feat: add filtering state and function to the context

--- client/src/pages/Wishlist/context/WishlistProvider.tsx
+++ client/src/pages/Wishlist/context/WishlistProvider.tsx
@@ -1,7 +1,18 @@
-import { JSX, ReactNode, useMemo, useState } from 'react';
-import WishlistContext, { WishlistContextType } from './WishlistContext';
+import { JSX, ReactNode, useCallback, useMemo, useState } from 'react';
+import WishlistContext, { ItemsFilterConfig, WishlistContextType } from './WishlistContext';
 import { WishlistDetailsType } from '../../../types/wishlistTypes';
 import { WishlistItemType } from '../../../types/wishlistItemTypes';
+
+const defaultItemsFilterConfig: ItemsFilterConfig = {
+  addedAfterTimestamp: null,
+  addedBeforeTimestamp: null,
+
+  isPurchased: null,
+  hasLink: null,
+
+  titleQuery: '',
+  tagsSet: new Set(),
+};
 
 export default function WishlistProvider({
   initialWishlistId,
@@ -19,10 +30,44 @@ export default function WishlistProvider({
   const [wishlistId, setWishlistId] = useState<string>(initialWishlistId);
   const [wishlistDetails, setWishlistDetails] = useState<WishlistDetailsType>(initialWishlistDetails);
   const [wishlistItems, setWishlistItems] = useState<WishlistItemType[]>(initialWishlistItems);
+  const [itemsFilterConfig, setItemsFilterConfig] = useState<ItemsFilterConfig>(defaultItemsFilterConfig);
 
   const wishlistItemsTitleSet: Set<string> = useMemo(
     () => new Set(wishlistItems.map((item: WishlistItemType) => item.title.toLowerCase())),
     [wishlistItems]
+  );
+
+  const itemMatchesFilterConfig = useCallback(
+    (item: WishlistItemType): boolean => {
+      const { addedAfterTimestamp, addedBeforeTimestamp, isPurchased, hasLink, titleQuery, tagsSet } = itemsFilterConfig;
+
+      if (addedAfterTimestamp) {
+        return item.added_on_timestamp < addedAfterTimestamp;
+      }
+
+      if (addedBeforeTimestamp) {
+        return item.added_on_timestamp > addedBeforeTimestamp;
+      }
+
+      if (isPurchased !== null) {
+        return item.is_purchased === isPurchased;
+      }
+
+      if (hasLink !== null) {
+        return Boolean(item.link) === hasLink;
+      }
+
+      if (!item.title.includes(titleQuery)) {
+        return false;
+      }
+
+      if (tagsSet.size === 0) {
+        return true;
+      }
+
+      return item.tags.some(({ name }) => tagsSet.has(name));
+    },
+    [itemsFilterConfig]
   );
 
   const contextValue: WishlistContextType = useMemo(
@@ -37,8 +82,11 @@ export default function WishlistProvider({
       setWishlistItems,
 
       wishlistItemsTitleSet,
+
+      setItemsFilterConfig,
+      itemMatchesFilterConfig,
     }),
-    [wishlistId, wishlistDetails, wishlistItems, wishlistItemsTitleSet]
+    [wishlistId, wishlistDetails, wishlistItems, wishlistItemsTitleSet, itemMatchesFilterConfig]
   );
 
   return <WishlistContext.Provider value={contextValue}>{children}</WishlistContext.Provider>;
