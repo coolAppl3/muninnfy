9081f257f71b369a2650b980597816558d1a7dae
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-10-03 01:58:30 +0200

    refactor: remove default prop value for className

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -8,15 +8,15 @@ type ButtonProps = {
   onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
-export default function Button({ children, className = '', disabled = false, isSubmitBtn = false, onClick }: ButtonProps): JSX.Element {
+export default function Button({ children, className, disabled = false, isSubmitBtn = false, onClick }: ButtonProps): JSX.Element {
   return (
     <button
       type={isSubmitBtn ? 'submit' : 'button'}
       disabled={disabled}
       onClick={onClick}
-      className={`block min-w-fit py-1 px-[2.4rem] font-bold text-base text-center leading-[2.2rem] rounded border-1 transition-[filter] ease-out ${className} ${
-        disabled ? 'opacity-25 cursor-default hover:brightness-100' : 'cursor-pointer hover:brightness-75'
-      }`}
+      className={`block min-w-fit py-1 px-[2.4rem] font-bold text-base text-center leading-[2.2rem] rounded border-1 transition-[filter] ease-out ${
+        className || ''
+      } ${disabled ? 'opacity-25 cursor-default hover:brightness-100' : 'cursor-pointer hover:brightness-75'}`}
     >
       {children}
     </button>
--- client/src/components/Container/Container.tsx
+++ client/src/components/Container/Container.tsx
@@ -1,5 +1,5 @@
 import { JSX, ReactNode } from 'react';
 
-export default function Container({ children, className = '' }: { children: ReactNode; className?: string }): JSX.Element {
-  return <div className={`max-w-[1200px] mx-auto px-2 h-full ${className}`}>{children}</div>;
+export default function Container({ children, className }: { children: ReactNode; className?: string }): JSX.Element {
+  return <div className={`max-w-[1200px] mx-auto px-2 h-full ${className || ''}`}>{children}</div>;
 }
