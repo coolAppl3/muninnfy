10844c175696a4a1176a72c5d63560bd97333bb9
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-10-04 20:13:17 +0200

    feat: add WishlistItemButtonContainer component, extracting the button container from WishlistItem, to improve readability and maintainability

--- client/src/pages/Wishlist/WishlistItems/WishlistItem/WishlistItem.tsx
+++ client/src/pages/Wishlist/WishlistItems/WishlistItem/WishlistItem.tsx
@@ -1,111 +1,17 @@
-import { FocusEvent, JSX, useState } from 'react';
+import { JSX, useState } from 'react';
 import { getShortenedDateString } from '../../../../utils/globalUtils';
 import ChevronIcon from '../../../../assets/svg/ChevronIcon.svg?react';
-import TripleDotMenuIcon from '../../../../assets/svg/TripleDotMenuIcon.svg?react';
-import CheckIcon from '../../../../assets/svg/CheckIcon.svg?react';
 import WishlistItemForm from '../../components/WishlistItemForm';
-import useLoadingOverlay from '../../../../hooks/useLoadingOverlay';
-import { deleteWishlistItemService, setWishlistItemIsPurchasedService } from '../../../../services/wishlistItemServices';
-import useWishlist from '../../context/useWishlist';
-import usePopupMessage from '../../../../hooks/usePopupMessage';
-import useHistory from '../../../../hooks/useHistory';
-import { NavigateFunction, useNavigate } from 'react-router-dom';
-import useAsyncErrorHandler, { HandleAsyncErrorFunction } from '../../../../hooks/useAsyncErrorHandler';
 import { WishlistItemType } from '../../../../types/wishlistItemTypes';
-import useConfirmModal from '../../../../hooks/useConfirmModal';
+import WishlistItemButtonContainer from './components/WishlistItemButtonContainer/WishlistItemButtonContainer';
 
 type WishlistItemProps = {
   wishlistItem: WishlistItemType;
 };
 
 export default function WishlistItem({ wishlistItem }: WishlistItemProps): JSX.Element {
-  const { wishlistId, setWishlistItems } = useWishlist();
-
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
-  const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false);
   const [isEditing, setIsEditing] = useState<boolean>(false);
-  const [updatingPurchaseStatus, setUpdatingPurchaseState] = useState<boolean>(false);
-
-  const handleAsyncError: HandleAsyncErrorFunction = useAsyncErrorHandler();
-  const { referrerLocation } = useHistory();
-  const navigate: NavigateFunction = useNavigate();
-  const { displayLoadingOverlay, removeLoadingOverlay } = useLoadingOverlay();
-  const { displayPopupMessage } = usePopupMessage();
-  const { displayConfirmModal, removeConfirmModal } = useConfirmModal();
-
-  async function setWishlistItemIsPurchased(): Promise<void> {
-    setUpdatingPurchaseState(true);
-
-    try {
-      await setWishlistItemIsPurchasedService({ wishlistId, itemId: wishlistItem.item_id, newPurchaseStatus: !wishlistItem.is_purchased });
-      setWishlistItems((prev) =>
-        prev.map((item: WishlistItemType) => {
-          if (item.item_id !== wishlistItem.item_id) {
-            return item;
-          }
-
-          return {
-            ...item,
-            is_purchased: !wishlistItem.is_purchased,
-          };
-        })
-      );
-    } catch (err: unknown) {
-      console.log(err);
-      const { isHandled, status, errReason } = handleAsyncError(err);
-
-      if (isHandled) {
-        return;
-      }
-
-      if (status === 400) {
-        displayPopupMessage('Something went wrong.', 'error');
-        return;
-      }
-
-      if (status !== 404) {
-        return;
-      }
-
-      if (errReason === 'wishlistNotFound') {
-        navigate(referrerLocation || '/account');
-        return;
-      }
-
-      setWishlistItems((prev) => prev.filter((item: WishlistItemType) => item.item_id !== wishlistItem.item_id));
-    } finally {
-      setUpdatingPurchaseState(false);
-    }
-  }
-
-  async function removeWishlistItem(): Promise<void> {
-    displayLoadingOverlay();
-
-    try {
-      await deleteWishlistItemService(wishlistId, wishlistItem.item_id);
-      setWishlistItems((prev) => prev.filter((item: WishlistItemType) => item.item_id !== wishlistItem.item_id));
-
-      displayPopupMessage('Item removed.', 'success');
-    } catch (err: unknown) {
-      console.log(err);
-      const { isHandled, status } = handleAsyncError(err);
-
-      if (isHandled) {
-        return;
-      }
-
-      if (status === 400) {
-        displayPopupMessage('Something went wrong.', 'error');
-        return;
-      }
-
-      if (status === 404) {
-        navigate(referrerLocation || '/account');
-      }
-    } finally {
-      removeLoadingOverlay();
-    }
-  }
 
   if (isEditing) {
     return (
@@ -120,7 +26,7 @@ export default function WishlistItem({ wishlistItem }: WishlistItemProps): JSX.E
   }
 
   return (
-    <div className={`bg-secondary rounded-sm shadow-simple-tiny ${isExpanded ? 'expanded' : ''}`}>
+    <div className='bg-secondary rounded-sm shadow-simple-tiny'>
       <button
         onClick={() => setIsExpanded((prev) => !prev)}
         tabIndex={0}
@@ -140,7 +46,7 @@ export default function WishlistItem({ wishlistItem }: WishlistItemProps): JSX.E
         </span>
       </button>
 
-      <div className={isExpanded ? 'flex justify-between items-start gap-1 p-2 pt-1' : 'hidden'}>
+      <div className={`justify-between items-start gap-1 p-2 pt-1 ${isExpanded ? 'flex' : 'hidden'}`}>
         <div className='w-full text-sm text-description grid gap-1'>
           <div className='pr-1 whitespace-nowrap overflow-hidden text-ellipsis'>
             <p>Added: {getShortenedDateString(wishlistItem.added_on_timestamp)}</p>
@@ -180,82 +86,10 @@ export default function WishlistItem({ wishlistItem }: WishlistItemProps): JSX.E
           )}
         </div>
 
-        <div
-          className={`grid gap-1 mr-[-1rem] relative place-items-center ${menuIsOpen ? 'open' : ''}`}
-          onBlur={(e: FocusEvent<HTMLDivElement>) => {
-            if (e.relatedTarget?.classList.contains('context-menu-btn')) {
-              return;
-            }
-
-            setMenuIsOpen(false);
-          }}
-        >
-          <button
-            type='button'
-            className={`p-1 rounded-[50%] transition-colors cursor-pointer hover:bg-dark mt-[-8px] ${
-              menuIsOpen ? 'bg-dark text-cta' : 'text-title'
-            }`}
-            title='Menu'
-            aria-label='Menu'
-            onClick={() => setMenuIsOpen((prev) => !prev)}
-          >
-            <TripleDotMenuIcon className='w-[1.6rem] h-[1.6rem] rotate-180' />
-          </button>
-
-          {updatingPurchaseStatus ? (
-            <div className='spinner w-[2.6rem] h-[2.6rem] my-[5px]'></div>
-          ) : (
-            <button
-              type='button'
-              className={`p-1 rounded-[50%] transition-colors cursor-pointer  ${
-                wishlistItem.is_purchased ? 'bg-cta hover:bg-cta/75' : 'bg-light/50 hover:bg-light'
-              }`}
-              title={`Mark as ${wishlistItem.is_purchased ? 'purchased' : 'unpurchased'}`}
-              aria-label={`Mark as ${wishlistItem.is_purchased ? 'purchased' : 'unpurchased'}`}
-              onClick={async () => await setWishlistItemIsPurchased()}
-            >
-              <CheckIcon className='w-[1.6rem] h-[1.6rem] text-dark' />
-            </button>
-          )}
-
-          <div
-            className={`absolute top-[-1rem] right-4 rounded-sm overflow-hidden shadow-centered-tiny ${menuIsOpen ? 'block' : 'hidden'}`}
-          >
-            <button
-              type='button'
-              className='context-menu-btn'
-              onClick={() => {
-                setMenuIsOpen(false);
-                setIsEditing(true);
-              }}
-            >
-              Edit
-            </button>
-
-            <button
-              type='button'
-              className='context-menu-btn danger'
-              onClick={() =>
-                displayConfirmModal({
-                  title: 'Are you sure you want to remove this item:',
-                  description: wishlistItem.title,
-                  confirmBtnTitle: 'Remove item',
-                  cancelBtnTitle: 'Cancel',
-                  isDangerous: true,
-                  onConfirm: async () => {
-                    removeConfirmModal();
-                    setMenuIsOpen(false);
-
-                    await removeWishlistItem();
-                  },
-                  onCancel: removeConfirmModal,
-                })
-              }
-            >
-              Remove
-            </button>
-          </div>
-        </div>
+        <WishlistItemButtonContainer
+          wishlistItem={wishlistItem}
+          setIsEditing={setIsEditing}
+        />
       </div>
     </div>
   );
---
+++ client/src/pages/Wishlist/WishlistItems/WishlistItem/components/WishlistItemButtonContainer/WishlistItemButtonContainer.tsx
@@ -0,0 +1,182 @@
+import { Dispatch, FocusEvent, JSX, SetStateAction, useState } from 'react';
+import useWishlist from '../../../../context/useWishlist';
+import useAsyncErrorHandler, { HandleAsyncErrorFunction } from '../../../../../../hooks/useAsyncErrorHandler';
+import useHistory from '../../../../../../hooks/useHistory';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
+import useLoadingOverlay from '../../../../../../hooks/useLoadingOverlay';
+import usePopupMessage from '../../../../../../hooks/usePopupMessage';
+import useConfirmModal from '../../../../../../hooks/useConfirmModal';
+import { WishlistItemType } from '../../../../../../types/wishlistItemTypes';
+import TripleDotMenuIcon from '../../../../../../assets/svg/TripleDotMenuIcon.svg?react';
+import CheckIcon from '../../../../../../assets/svg/CheckIcon.svg?react';
+import { deleteWishlistItemService, setWishlistItemIsPurchasedService } from '../../../../../../services/wishlistItemServices';
+
+type WishlistItemButtonContainerProps = {
+  wishlistItem: WishlistItemType;
+  setIsEditing: Dispatch<SetStateAction<boolean>>;
+};
+
+export default function WishlistItemButtonContainer({ wishlistItem, setIsEditing }: WishlistItemButtonContainerProps): JSX.Element {
+  const { wishlistId, setWishlistItems } = useWishlist();
+
+  const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false);
+  const [updatingPurchaseStatus, setUpdatingPurchaseState] = useState<boolean>(false);
+
+  const handleAsyncError: HandleAsyncErrorFunction = useAsyncErrorHandler();
+  const { referrerLocation } = useHistory();
+  const navigate: NavigateFunction = useNavigate();
+  const { displayLoadingOverlay, removeLoadingOverlay } = useLoadingOverlay();
+  const { displayPopupMessage } = usePopupMessage();
+  const { displayConfirmModal, removeConfirmModal } = useConfirmModal();
+
+  async function setWishlistItemIsPurchased(): Promise<void> {
+    setUpdatingPurchaseState(true);
+
+    try {
+      await setWishlistItemIsPurchasedService({ wishlistId, itemId: wishlistItem.item_id, newPurchaseStatus: !wishlistItem.is_purchased });
+      setWishlistItems((prev) =>
+        prev.map((item: WishlistItemType) => {
+          if (item.item_id !== wishlistItem.item_id) {
+            return item;
+          }
+
+          return {
+            ...item,
+            is_purchased: !wishlistItem.is_purchased,
+          };
+        })
+      );
+    } catch (err: unknown) {
+      console.log(err);
+      const { isHandled, status, errReason } = handleAsyncError(err);
+
+      if (isHandled) {
+        return;
+      }
+
+      if (status === 400) {
+        displayPopupMessage('Something went wrong.', 'error');
+        return;
+      }
+
+      if (status !== 404) {
+        return;
+      }
+
+      if (errReason === 'wishlistNotFound') {
+        navigate(referrerLocation || '/account');
+        return;
+      }
+
+      setWishlistItems((prev) => prev.filter((item: WishlistItemType) => item.item_id !== wishlistItem.item_id));
+    } finally {
+      setUpdatingPurchaseState(false);
+    }
+  }
+
+  async function removeWishlistItem(): Promise<void> {
+    displayLoadingOverlay();
+
+    try {
+      await deleteWishlistItemService(wishlistId, wishlistItem.item_id);
+      setWishlistItems((prev) => prev.filter((item: WishlistItemType) => item.item_id !== wishlistItem.item_id));
+
+      displayPopupMessage('Item removed.', 'success');
+    } catch (err: unknown) {
+      console.log(err);
+      const { isHandled, status } = handleAsyncError(err);
+
+      if (isHandled) {
+        return;
+      }
+
+      if (status === 400) {
+        displayPopupMessage('Something went wrong.', 'error');
+        return;
+      }
+
+      if (status === 404) {
+        navigate(referrerLocation || '/account');
+      }
+    } finally {
+      removeLoadingOverlay();
+    }
+  }
+
+  return (
+    <div
+      className='grid gap-1 mr-[-1rem] relative place-items-center'
+      onBlur={(e: FocusEvent<HTMLDivElement>) => {
+        if (e.relatedTarget?.classList.contains('context-menu-btn')) {
+          return;
+        }
+
+        setMenuIsOpen(false);
+      }}
+    >
+      <button
+        type='button'
+        className={`p-1 rounded-[50%] transition-colors cursor-pointer hover:bg-dark mt-[-8px] ${
+          menuIsOpen ? 'bg-dark text-cta' : 'text-title'
+        }`}
+        title='Menu'
+        aria-label='Menu'
+        onClick={() => setMenuIsOpen((prev) => !prev)}
+      >
+        <TripleDotMenuIcon className='w-[1.6rem] h-[1.6rem] rotate-180' />
+      </button>
+
+      {updatingPurchaseStatus ? (
+        <div className='spinner w-[2.6rem] h-[2.6rem] my-[5px]'></div>
+      ) : (
+        <button
+          type='button'
+          className={`p-1 rounded-[50%] transition-colors cursor-pointer  ${
+            wishlistItem.is_purchased ? 'bg-cta hover:bg-cta/75' : 'bg-light/50 hover:bg-light'
+          }`}
+          title={`Mark as ${wishlistItem.is_purchased ? 'purchased' : 'unpurchased'}`}
+          aria-label={`Mark as ${wishlistItem.is_purchased ? 'purchased' : 'unpurchased'}`}
+          onClick={async () => await setWishlistItemIsPurchased()}
+        >
+          <CheckIcon className='w-[1.6rem] h-[1.6rem] text-dark' />
+        </button>
+      )}
+
+      <div className={`absolute top-[-1rem] right-4 rounded-sm overflow-hidden shadow-centered-tiny ${menuIsOpen ? 'block' : 'hidden'}`}>
+        <button
+          type='button'
+          className='context-menu-btn'
+          onClick={() => {
+            setMenuIsOpen(false);
+            setIsEditing(true);
+          }}
+        >
+          Edit
+        </button>
+
+        <button
+          type='button'
+          className='context-menu-btn danger'
+          onClick={() =>
+            displayConfirmModal({
+              title: 'Are you sure you want to remove this item:',
+              description: wishlistItem.title,
+              confirmBtnTitle: 'Remove item',
+              cancelBtnTitle: 'Cancel',
+              isDangerous: true,
+              onConfirm: async () => {
+                removeConfirmModal();
+                setMenuIsOpen(false);
+
+                await removeWishlistItem();
+              },
+              onCancel: removeConfirmModal,
+            })
+          }
+        >
+          Remove
+        </button>
+      </div>
+    </div>
+  );
+}
