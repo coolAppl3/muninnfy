4fda91f567bbab194de4e8f38f9e3a8a6cdc99cd
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-10-05 23:02:30 +0200

    improvement: improve margins

--- client/src/pages/Wishlist/WishlistItemsToolbar/components/WishlistItemsToolbarFilters/WishlistItemsToolbarFilters.tsx
+++ client/src/pages/Wishlist/WishlistItemsToolbar/components/WishlistItemsToolbarFilters/WishlistItemsToolbarFilters.tsx
@@ -13,32 +13,33 @@ export default function WishlistItemsToolbarFilters({ isOpen, setIsOpen }: Wishl
   const [filterByLink, setFilterByLink] = useState<boolean | null>(null);
 
   return (
-    <div className={`bg-secondary p-2 rounded-sm shadow-simple-tiny mb-2 ${isOpen ? 'block' : 'hidden'}`}>
-      <h4 className='text-title mb-2'>Filters</h4>
+    <div className={`grid gap-2 bg-secondary p-2 rounded-sm shadow-simple-tiny mb-2 ${isOpen ? 'block' : 'hidden'}`}>
+      <h4 className='text-title'>Filters</h4>
+
+      <div className='grid gap-1'>
+        <WishlistItemsToolbarFilterItem
+          filterBy={filterByPurchaseStatus}
+          setFilterBy={setFilterByPurchaseStatus}
+          title='Purchase status'
+          positiveFilterTitle='Purchased'
+          negativeFilterTitle='Unpurchased'
+        />
+
+        <WishlistItemsToolbarFilterItem
+          filterBy={filterByLink}
+          setFilterBy={setFilterByLink}
+          title='Link'
+          positiveFilterTitle='Contains a link'
+          negativeFilterTitle={`Doesn't contain a link`}
+        />
+      </div>
 
       <TimeWindowContainer
         startLabel='Added after'
         endLabel='Added before'
-        className='mb-2'
       />
 
-      <WishlistItemsToolbarFilterItem
-        filterBy={filterByPurchaseStatus}
-        setFilterBy={setFilterByPurchaseStatus}
-        title='Purchase status'
-        positiveFilterTitle='Purchased'
-        negativeFilterTitle='Unpurchased'
-      />
-
-      <WishlistItemsToolbarFilterItem
-        filterBy={filterByLink}
-        setFilterBy={setFilterByLink}
-        title='Link'
-        positiveFilterTitle='Contains a link'
-        negativeFilterTitle={`Doesn't contain a link`}
-      />
-
-      <div className='btn-container flex flex-col sm:flex-row justify-start items-start gap-1 mt-2'>
+      <div className='btn-container flex flex-col sm:flex-row justify-start items-start gap-1'>
         <Button className='bg-cta border-cta w-full sm:w-fit order-1 sm:order-3'>Apply filters</Button>
         <Button className='bg-secondary border-title text-title w-full sm:w-fit order-2'>Cancel</Button>
         <Button className='bg-description border-description w-full sm:w-fit order-3 sm:order-1 mt-1 sm:mt-0'>Reset</Button>
