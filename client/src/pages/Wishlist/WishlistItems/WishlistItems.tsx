4895e4bc233cc43e95f4edf6d8581f6b776931ba
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-10-21 03:03:33 +0200

    fix: move Wrapper outside WishlistItems component to prevent it from being recreated on every rerender

--- client/src/pages/Wishlist/WishlistItems/WishlistItems.tsx
+++ client/src/pages/Wishlist/WishlistItems/WishlistItems.tsx
@@ -1,29 +1,21 @@
-import { JSX, ReactNode, useMemo } from 'react';
+import { JSX, Profiler, ReactNode, useMemo } from 'react';
 import Container from '../../../components/Container/Container';
 import WishlistItem from './WishlistItem/WishlistItem';
 import { WishlistItemType } from '../../../types/wishlistItemTypes';
 import useWishlistItems from '../hooks/useWishlistItems';
 
 export default function WishlistItems(): JSX.Element {
-  const { wishlistItems, itemMatchesFilterConfig, wishlistItemsLoading, isSingleColumnView } = useWishlistItems();
+  const { wishlistItems, itemMatchesFilterConfig, wishlistItemsLoading } = useWishlistItems();
 
   const filteredItems: WishlistItemType[] = useMemo(
     () => wishlistItems.filter(itemMatchesFilterConfig),
     [wishlistItems, itemMatchesFilterConfig]
   );
 
-  const Wrapper = ({ children }: { children: ReactNode }): JSX.Element => (
-    <section>
-      <Container>
-        <div className={`grid grid-cols-1 ${isSingleColumnView ? '' : 'sm:grid-cols-2'} gap-1 items-start`}>{children}</div>
-      </Container>
-    </section>
-  );
-
   if (wishlistItemsLoading) {
     return (
       <Wrapper>
-        <div className='spinner col-span-2 w-3 h-3 mx-auto'></div>{' '}
+        <div className='spinner col-span-2 w-3 h-3 mx-auto'></div>
       </Wrapper>
     );
   }
@@ -45,5 +37,17 @@ export default function WishlistItems(): JSX.Element {
         />
       ))}
     </Wrapper>
+  );
+}
+
+function Wrapper({ children }: { children: ReactNode }): JSX.Element {
+  const { isSingleColumnView } = useWishlistItems();
+
+  return (
+    <section>
+      <Container>
+        <div className={`grid grid-cols-1 ${isSingleColumnView ? '' : 'sm:grid-cols-2'} gap-1 items-start`}>{children}</div>
+      </Container>
+    </section>
   );
 }
