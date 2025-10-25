4a94c1ab3a7f7c2ca0a35fb0106b6bea01e9d416
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-10-26 01:45:24 +0200

    change: is_purchased column in wishlist_items table to purchased_on_timestamp

--- src/db/initDb.ts
+++ src/db/initDb.ts
@@ -174,7 +174,7 @@ async function createWishlistItemsTable(): Promise<void> {
         title VARCHAR(100) NOT NULL,
         description VARCHAR(600),
         link VARCHAR(2048),
-        is_purchased BOOLEAN NOT NULL,
+        purchased_on_timestamp BIGINT UNSIGNED,
         FOREIGN KEY (wishlist_id) REFERENCES wishlists(wishlist_id) ON DELETE CASCADE,
         UNIQUE(title, wishlist_id)
       );`
