699f9b7dccc5f8672ef0e733e3ea33ae87443f35
coolAppl3 <yanal.mohsen@yahoo.com>
2025-11-07 21:18:38 +0100

    change: reduce TOTAL_WISHLISTS_LIMIT to 100

--- src/util/constants/wishlistConstants.ts
+++ src/util/constants/wishlistConstants.ts
@@ -2,7 +2,7 @@ export const PRIVATE_WISHLIST_PRIVACY_LEVEL: number = 0;
 export const FOLLOWERS_WISHLIST_PRIVACY_LEVEL: number = 1;
 export const PUBLIC_WISHLIST_PRIVACY_LEVEL: number = 2;
 
-export const TOTAL_WISHLISTS_LIMIT: number = 10_000;
+export const TOTAL_WISHLISTS_LIMIT: number = 100;
 export const WISHLIST_ITEMS_LIMIT: number = 100;
 
 export const WISHLIST_FETCH_BATCH_SIZE: number = 20;
