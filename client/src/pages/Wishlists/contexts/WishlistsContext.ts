80f2b5aa72835c92d73e03864a6c83aa77d15e12
coolAppl3 <yanal.mohsen@yahoo.com>
2025-11-09 02:07:12 +0100

    feat: add WishlistsContext, WishlistsProvider, and useWishlists custom hook

---
+++ client/src/pages/Wishlists/contexts/WishlistsContext.ts
@@ -0,0 +1,40 @@
+import { createContext, Dispatch, SetStateAction } from 'react';
+import { ExtendedWishlistDetailsType } from '../../../types/wishlistTypes';
+
+export type WishlistsFilterConfigType = {
+  createdAfterTimestamp: number | null;
+  createdBeforeTimestamp: number | null;
+
+  itemsCountFrom: number | null;
+  itemsCountTo: number | null;
+
+  totalItemsPriceFrom: number | null;
+  totalItemsPriceTo: number | null;
+
+  priceToCompleteFrom: number | null;
+  priceToCompleteTo: number | null;
+
+  isCompleted: boolean | null;
+  titleQuery: string;
+};
+
+export type WishlistsSortingMode = 'newest_first' | 'oldest_first' | 'largest_first' | 'smallest_first' | 'lexicographical';
+
+export type WishlistsContextType = {
+  wishlists: ExtendedWishlistDetailsType[];
+  setWishlists: Dispatch<SetStateAction<ExtendedWishlistDetailsType[]>>;
+
+  wishlistsFilterConfig: WishlistsFilterConfigType;
+  setWishlistsFilterConfig: Dispatch<SetStateAction<WishlistsFilterConfigType>>;
+  wishlistMatchesFilterConfig: (wishlist: ExtendedWishlistDetailsType) => boolean;
+
+  wishlistsSortingMode: WishlistsSortingMode;
+  setWishlistsSortingMode: Dispatch<SetStateAction<WishlistsSortingMode>>;
+  sortWishlists: (explicitSortingMode?: WishlistsSortingMode) => void;
+
+  isSingleColumnView: boolean;
+  setIsSingleColumnView: Dispatch<SetStateAction<boolean>>;
+};
+
+const WishlistsContext = createContext<WishlistsContextType | null>(null);
+export default WishlistsContext;
