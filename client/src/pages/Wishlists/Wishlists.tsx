6ad68c464e2c5549ca82ad097796be457263e185
coolAppl3 <yanal.mohsen@yahoo.com>
2025-11-10 01:41:31 +0100

    fix: useEffect not rerouting the user if fetching the wishlists fails

--- client/src/pages/Wishlists/Wishlists.tsx
+++ client/src/pages/Wishlists/Wishlists.tsx
@@ -8,12 +8,16 @@ import LoadingSkeleton from '../../components/LoadingSkeleton/LoadingSkeleton';
 import WishlistsProvider from './providers/WishlistsProvider';
 import WishlistsContainer from './WishlistsContainer/WishlistsContainer';
 import { WishlistsToolbar } from './WishlistsToolbar/WishlistsToolbar';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
+import useHistory from '../../hooks/useHistory';
 
 export default function Wishlists(): JSX.Element {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [wishlists, setWishlists] = useState<ExtendedWishlistDetailsType[]>([]);
 
   const handleAsyncError: HandleAsyncErrorFunction = useAsyncErrorHandler();
+  const navigate: NavigateFunction = useNavigate();
+  const { referrerLocation } = useHistory();
 
   useEffect(() => {
     const abortController: AbortController = new AbortController();
@@ -34,7 +38,11 @@ export default function Wishlists(): JSX.Element {
         }
 
         console.log(err);
-        handleAsyncError(err);
+        const { isHandled, status } = handleAsyncError(err);
+
+        if (!isHandled || status !== 401) {
+          navigate(referrerLocation || '/account');
+        }
       }
     };
 
