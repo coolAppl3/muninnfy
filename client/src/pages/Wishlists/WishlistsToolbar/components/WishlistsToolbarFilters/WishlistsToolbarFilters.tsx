70b7c2b83980995e3c156834f74cfa91b02a3183
coolAppl3 <yanal.mohsen@yahoo.com>
2025-11-13 03:55:31 +0100

    feat: implement WishlistsToolbarFilters (optimizations to be added in future commits)

--- client/src/pages/Wishlists/WishlistsToolbar/components/WishlistsToolbarFilters/WishlistsToolbarFilters.tsx
+++ client/src/pages/Wishlists/WishlistsToolbar/components/WishlistsToolbarFilters/WishlistsToolbarFilters.tsx
@@ -5,7 +5,9 @@ import useWishlists from '../../../hooks/useWishlists';
 import useCalendar from '../../../../../hooks/useCalendar';
 import PriceRangeFormGroup from '../../../../../components/PriceRangeFormGroup/PriceRangeFormGroup';
 import { WISHLIST_MAX_PRICE_TO_COMPLETE } from '../../../../../utils/constants/wishlistConstants';
-import FilterToggler from '../../../../../components/FilterToggler/FilterToggler';
+import ToggleSwitch from '../../../../../components/ToggleSwitch/ToggleSwitch';
+import usePopupMessage from '../../../../../hooks/usePopupMessage';
+import WishlistsItemsCountRange from './components/WishlistsItemsCountRange';
 
 type WishlistsToolbarFiltersProps = {
   isOpen: boolean;
@@ -19,24 +21,25 @@ export default function WishlistsToolbarFilters({ isOpen, setIsOpen }: Wishlists
   const [createdAfterTimestamp, setCreatedAfterTimestamp] = useState<number | null>(null);
   const [createdBeforeTimestamp, setCreatedBeforeTimestamp] = useState<number | null>(null);
 
-  const [filterByItemsCount, setFilterByItemsCount] = useState<boolean | null>(null);
+  const [filterByItemsCount, setFilterByItemsCount] = useState<boolean>(false);
   const [itemsCountFrom, setItemsCountFrom] = useState<number | null>(null);
   const [itemsCountTo, setItemsCountTo] = useState<number | null>(null);
   const [itemsCountRangeValid, setItemsCountRangeValid] = useState<boolean>(true);
 
-  const [filterByTotalItemsPrice, setFilterByTotalItemsPrice] = useState<boolean | null>(null);
+  const [filterByTotalItemsPrice, setFilterByTotalItemsPrice] = useState<boolean>(false);
   const [totalItemsPriceFrom, setTotalIItemsFrom] = useState<number | null>(null);
   const [totalItemsPriceTo, setTotalIItemsTo] = useState<number | null>(null);
   const [totalItemsPriceRangeValid, setTotalItemsPriceRangeValid] = useState<boolean>(true);
 
-  const [filterByPriceToComplete, setFilterByPriceToComplete] = useState<boolean | null>(null);
+  const [filterByPriceToComplete, setFilterByPriceToComplete] = useState<boolean>(false);
   const [priceToCompleteFrom, setPriceToCompleteFrom] = useState<number | null>(null);
   const [priceToCompleteTo, setPriceToCompleteTo] = useState<number | null>(null);
   const [priceToCompleteRangeValid, setPriceToCompleteRangeValid] = useState<boolean>(true);
 
-  const [isCompleted, setIsCompleted] = useState<boolean | null>(null);
-
+  const allRangesValid: boolean = itemsCountRangeValid && totalItemsPriceRangeValid && priceToCompleteRangeValid;
   const createdTimestampsKey: string = 'createdTimestamps';
+
+  const { displayPopupMessage } = usePopupMessage();
 
   useEffect(() => {
     setCreatedAfterTimestamp(startTimestampsMap.get(createdTimestampsKey) || null);
@@ -44,15 +47,78 @@ export default function WishlistsToolbarFilters({ isOpen, setIsOpen }: Wishlists
   }, [startTimestampsMap, endTimestampsMap]);
 
   function changesDetected(): boolean {
+    if (!allRangesValid) {
+      return false;
+    }
+
+    const stateRecord = {
+      createdAfterTimestamp,
+      createdBeforeTimestamp,
+      itemsCountFrom,
+      itemsCountTo,
+      totalItemsPriceFrom,
+      totalItemsPriceTo,
+      priceToCompleteFrom,
+      priceToCompleteTo,
+    };
+
+    for (const key of Object.keys(stateRecord) as (keyof typeof stateRecord)[]) {
+      if (stateRecord[key] !== wishlistsFilterConfig[key]) {
+        return true;
+      }
+    }
+
     return false;
   }
 
   function applyFilters(): void {
-    //
+    if (!allRangesValid) {
+      displayPopupMessage('Invalid filter configuration.', 'error');
+      return;
+    }
+
+    setWishlistsFilterConfig((prev) => ({
+      ...prev,
+
+      createdAfterTimestamp,
+      createdBeforeTimestamp,
+      itemsCountFrom,
+      itemsCountTo,
+      totalItemsPriceFrom,
+      totalItemsPriceTo,
+      priceToCompleteFrom,
+      priceToCompleteTo,
+    }));
+
+    displayPopupMessage('Filters applied.', 'success');
   }
 
   function resetFilter(): void {
-    //
+    setFilterByItemsCount(false);
+    setFilterByTotalItemsPrice(false);
+    setFilterByPriceToComplete(false);
+
+    setCreatedAfterTimestamp(null);
+    setCreatedBeforeTimestamp(null);
+    setItemsCountFrom(null);
+    setItemsCountTo(null);
+    setTotalIItemsFrom(null);
+    setTotalIItemsTo(null);
+    setPriceToCompleteFrom(null);
+    setPriceToCompleteTo(null);
+
+    setWishlistsFilterConfig((prev) => ({
+      ...prev,
+
+      createdAfterTimestamp: null,
+      createdBeforeTimestamp: null,
+      itemsCountFrom: null,
+      itemsCountTo: null,
+      totalItemsPriceFrom: null,
+      totalItemsPriceTo: null,
+      priceToCompleteFrom: null,
+      priceToCompleteTo: null,
+    }));
   }
 
   return (
@@ -72,50 +138,96 @@ export default function WishlistsToolbarFilters({ isOpen, setIsOpen }: Wishlists
       />
 
       <div className='grid gap-1'>
-        <FilterToggler
-          filterBy={filterByItemsCount}
-          setFilterBy={setFilterByItemsCount}
-          title='Items'
-          positiveFilterTitle={null}
-          negativeFilterTitle={null}
-        />
+        <div className='grid gap-1'>
+          <header className='flex justify-start items-center gap-1'>
+            <ToggleSwitch
+              isToggled={filterByItemsCount}
+              setIsToggled={() => {
+                const newValue: boolean = !filterByItemsCount;
+                setFilterByItemsCount(newValue);
 
-        {filterByItemsCount && <>{/* TODO: implement items count range */}</>}
+                if (newValue) {
+                  return;
+                }
 
-        <FilterToggler
-          filterBy={filterByTotalItemsPrice}
-          setFilterBy={setFilterByTotalItemsPrice}
-          title='Worth'
-          positiveFilterTitle={null}
-          negativeFilterTitle={null}
-        />
+                setItemsCountFrom(null);
+                setItemsCountTo(null);
+                setItemsCountRangeValid(true);
+              }}
+            />
+            <p className='text-title text-sm leading-[1]'>Items</p>
+          </header>
 
-        {filterByTotalItemsPrice && (
-          <PriceRangeFormGroup
-            setPriceFrom={setTotalIItemsFrom}
-            setPriceTo={setTotalIItemsTo}
-            setPriceRangeValid={setTotalItemsPriceRangeValid}
-            maxPrice={WISHLIST_MAX_PRICE_TO_COMPLETE}
-            className='mb-1'
-          />
-        )}
+          {filterByItemsCount && (
+            <WishlistsItemsCountRange
+              setCountFrom={setItemsCountFrom}
+              setCountTo={setItemsCountTo}
+              setCountRangeValid={setItemsCountRangeValid}
+              className='mb-1'
+            />
+          )}
+        </div>
 
-        <FilterToggler
-          filterBy={filterByPriceToComplete}
-          setFilterBy={setFilterByPriceToComplete}
-          title='To complete'
-          positiveFilterTitle={null}
-          negativeFilterTitle={null}
-        />
+        <div className='grid gap-1'>
+          <header className='flex justify-start items-center gap-1'>
+            <ToggleSwitch
+              isToggled={filterByTotalItemsPrice}
+              setIsToggled={() => {
+                const newValue: boolean = !filterByTotalItemsPrice;
+                setFilterByTotalItemsPrice(newValue);
 
-        {filterByPriceToComplete && (
-          <PriceRangeFormGroup
-            setPriceFrom={setPriceToCompleteFrom}
-            setPriceTo={setPriceToCompleteTo}
-            setPriceRangeValid={setPriceToCompleteRangeValid}
-            maxPrice={WISHLIST_MAX_PRICE_TO_COMPLETE}
-          />
-        )}
+                if (newValue) {
+                  return;
+                }
+
+                setTotalIItemsFrom(null);
+                setTotalIItemsTo(null);
+                setTotalItemsPriceRangeValid(true);
+              }}
+            />
+            <p className='text-title text-sm leading-[1]'>Worth</p>
+          </header>
+
+          {filterByTotalItemsPrice && (
+            <PriceRangeFormGroup
+              setPriceFrom={setTotalIItemsFrom}
+              setPriceTo={setTotalIItemsTo}
+              setPriceRangeValid={setTotalItemsPriceRangeValid}
+              maxPrice={WISHLIST_MAX_PRICE_TO_COMPLETE}
+              className='mb-1'
+            />
+          )}
+        </div>
+
+        <div className='grid gap-1'>
+          <header className='flex justify-start items-center gap-1'>
+            <ToggleSwitch
+              isToggled={filterByPriceToComplete}
+              setIsToggled={() => {
+                const newValue: boolean = !filterByPriceToComplete;
+                setFilterByPriceToComplete(newValue);
+
+                if (newValue) {
+                  return;
+                }
+
+                setPriceToCompleteFrom(null);
+                setPriceToCompleteTo(null);
+                setPriceToCompleteRangeValid(true);
+              }}
+            />
+            <p className='text-title text-sm leading-[1]'>To complete</p>
+          </header>
+
+          {filterByPriceToComplete && (
+            <PriceRangeFormGroup
+              setPriceFrom={setPriceToCompleteFrom}
+              setPriceTo={setPriceToCompleteTo}
+              setPriceRangeValid={setPriceToCompleteRangeValid}
+              maxPrice={WISHLIST_MAX_PRICE_TO_COMPLETE}
+            />
+          )}
+        </div>
       </div>
 
       <div className='btn-container flex flex-col sm:flex-row justify-start items-start gap-1'>
