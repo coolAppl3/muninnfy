ad755e7769acc0f9fb28e026c127096970c33718
coolAppl3 <yanal.mohsen@yahoo.com>
2025-11-27 20:59:38 +0100

    feat: implement AccountLocationProvider AccountSidebar, and AccountContent

--- client/src/pages/Account/Account.tsx
+++ client/src/pages/Account/Account.tsx
@@ -1,10 +1,23 @@
 import { JSX } from 'react';
 import Head from '../../components/Head/Head';
+import AccountSidebar from './AccountSidebar/AccountSidebar';
+import Container from '../../components/Container/Container';
+import AccountContent from './AccountContent/AccountContent';
+import AccountLocationProvider from './providers/AccountLocationProvider';
 
 export default function Account(): JSX.Element {
   return (
     <>
       <Head title='Account - Muninnfy' />
+
+      <AccountLocationProvider>
+        <main className='py-4'>
+          <Container className='grid grid-cols-12 gap-1'>
+            <AccountSidebar />
+            <AccountContent />
+          </Container>
+        </main>
+      </AccountLocationProvider>
     </>
   );
 }
