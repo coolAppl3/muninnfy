952c5be947fb5b816cf2a4362035e6266bb41d26
coolAppl3 <yanal.mohsen@yahoo.com>
2025-11-27 20:57:47 +0100

    feat: add stub for AccountContent component

---
+++ client/src/pages/Account/AccountContent/AccountContent.tsx
@@ -0,0 +1,5 @@
+import { JSX } from 'react';
+
+export default function AccountContent(): JSX.Element {
+  return <div className='p-2 bg-secondary rounded-sm col-span-9'>{/* TODO: continue implementation */}</div>;
+}
