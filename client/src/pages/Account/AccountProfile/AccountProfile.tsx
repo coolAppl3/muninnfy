a24a6b1d4b9135d5f2f164037a79c4d253e51d83
coolAppl3 <yanal.mohsen@yahoo.com>
2025-11-30 20:07:58 +0100

    feat: add stub for UI structure

--- client/src/pages/Account/AccountProfile/AccountProfile.tsx
+++ client/src/pages/Account/AccountProfile/AccountProfile.tsx
@@ -1,5 +1,38 @@
 import { JSX } from 'react';
+import StatisticItem from '../../../components/StatisticItem/StatisticItem';
 
 export function AccountProfile(): JSX.Element {
-  return <></>;
+  return (
+    <>
+      <h3
+        className='text-md text-title font-normal break-words mb-[8px]'
+        style={{ wordBreak: 'break-word' }}
+      >
+        Personal information
+      </h3>
+
+      <div className='text-description/50 text-xs mb-[1.2rem]'>
+        <p className='leading-[1] mb-[4px]'>e8bea0eb-140d-4e07-a13e-ec47a88311e5</p>
+        <p className='leading-[1]'>Created Sep 23rd, 2025</p>
+      </div>
+
+      <div className='grid md:grid-cols-2 gap-1 text-sm text-description'>
+        <StatisticItem
+          title='Display name'
+          value='John doe'
+        />
+
+        <StatisticItem
+          title='Username'
+          value='johnDoe23'
+        />
+
+        <StatisticItem
+          title='Email address'
+          value='johnDoe23@example.com'
+          className='md:col-span-2'
+        />
+      </div>
+    </>
+  );
 }
