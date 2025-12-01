328c03acddb7b81f58a7630cf805c092157829be
coolAppl3 <yanal.mohsen@yahoo.com>
2025-12-01 21:19:12 +0100

    feat: fill basic data with accountDetails

--- client/src/pages/Account/AccountProfile/AccountProfile.tsx
+++ client/src/pages/Account/AccountProfile/AccountProfile.tsx
@@ -2,31 +2,36 @@ import { JSX } from 'react';
 import StatisticItem from '../../../components/StatisticItem/StatisticItem';
 import AccountProfileHeader from './components/AccountProfileHeader/AccountProfileHeader';
 import AccountProfilePrivacy from './components/AccountProfilePrivacy/AccountProfilePrivacy';
+import useAccountDetails from '../hooks/useAccountDetails';
+import { getFullDateString } from '../../../utils/globalUtils';
 
 export function AccountProfile(): JSX.Element {
+  const { accountDetails } = useAccountDetails();
+  const { public_account_id, created_on_timestamp, display_name, username, email } = accountDetails;
+
   return (
     <>
       <AccountProfileHeader />
 
       <div className='text-description/50 text-xs mb-[1.2rem]'>
-        <p className='leading-[1] mb-[4px]'>e8bea0eb-140d-4e07-a13e-ec47a88311e5</p>
-        <p className='leading-[1]'>Created Sep 23rd, 2025</p>
+        <p className='leading-[1] mb-[4px]'>{public_account_id}</p>
+        <p className='leading-[1]'>Created on {getFullDateString(created_on_timestamp)}</p>
       </div>
 
       <div className='grid md:grid-cols-2 gap-1 text-sm text-description'>
         <StatisticItem
           title='Display name'
-          value='John doe'
+          value={display_name}
         />
 
         <StatisticItem
           title='Username'
-          value='johnDoe23'
+          value={username}
         />
 
         <StatisticItem
           title='Email address'
-          value='johnDoe23@example.com'
+          value={email}
           className='md:col-span-2'
         />
       </div>
