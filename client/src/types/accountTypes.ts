868f8421a0cfd9d3e0b89165c174973e5d25bf76
coolAppl3 <yanal.mohsen@yahoo.com>
2025-12-01 02:26:07 +0100

    feat: add accountTypes with AccountDetails

---
+++ client/src/types/accountTypes.ts
@@ -0,0 +1,9 @@
+export type AccountDetails = {
+  public_account_id: string;
+  email: string;
+  username: string;
+  display_name: string;
+  created_on_timestamp: number;
+  is_private: boolean;
+  approve_follow_requests: boolean;
+};
