7b5a4505534a2f13b2fe66a1f422d334a6f0375f
coolAppl3 <yanal.mohsen@yahoo.com>
2025-12-03 03:38:50 +0100

    improvement: add sendEmailService() locally and refactor for improved readability

--- src/util/email/emailServices.ts
+++ src/util/email/emailServices.ts
@@ -1,26 +1,38 @@
 import { emailTransporter } from './initTransporter';
 import accountVerificationEmailTemplate from './emailTemplates/accountVerificationEmailTemplate';
 
-type SendAccountVerificationEmailConfig = {
+type SendEmailService = {
+  receiver: string;
+  subject: string;
+  html: string;
+};
+
+async function sendEmailService({ receiver, subject, html }: SendEmailService): Promise<void> {
+  try {
+    await emailTransporter.sendMail({
+      from: `Muninnfy <${process.env.TRANSPORTER_USER}>`,
+      to: receiver,
+      subject,
+      html,
+    });
+  } catch (err: unknown) {
+    console.log(err);
+  }
+}
+
+type SendAccountVerificationEmailServicePayload = {
   receiver: string;
   displayName: string;
   publicAccountId: string;
   verificationToken: string;
 };
 
-export async function sendAccountVerificationEmailService(config: SendAccountVerificationEmailConfig): Promise<void> {
-  try {
-    await emailTransporter.sendMail({
-      from: `Muninnfy <${process.env.TRANSPORTER_USER}>`,
-      to: config.receiver,
-      subject: 'Muninnfy - Account Verification',
-      html: accountVerificationEmailTemplate({
-        displayName: config.displayName,
-        publicAccountId: config.publicAccountId,
-        verificationToken: config.verificationToken,
-      }),
-    });
-  } catch (err: unknown) {
-    console.log(err);
-  }
+export async function sendAccountVerificationEmailService(payload: SendAccountVerificationEmailServicePayload): Promise<void> {
+  const { receiver, ...rest } = payload;
+
+  await sendEmailService({
+    receiver,
+    subject: 'Muninnfy - Account Verification',
+    html: accountVerificationEmailTemplate({ ...rest }),
+  });
 }
