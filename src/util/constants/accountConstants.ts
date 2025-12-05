60e2047e048d3b8f4e33c4582f3468d4898e1442
coolAppl3 <yanal.mohsen@yahoo.com>
2025-12-06 00:00:14 +0100

    change: ACCOUNT_EMAIL_UPDATE_WINDOW to an hour down from a day

--- src/util/constants/accountConstants.ts
+++ src/util/constants/accountConstants.ts
@@ -6,5 +6,5 @@ export const ACCOUNT_EMAILS_SENT_LIMIT: number = 3;
 export const ACCOUNT_VERIFICATION_WINDOW: number = minuteMilliseconds * 20;
 export const ACCOUNT_RECOVERY_WINDOW: number = hourMilliseconds;
 export const ACCOUNT_DELETION_WINDOW: number = hourMilliseconds;
-export const ACCOUNT_EMAIL_UPDATE_WINDOW: number = dayMilliseconds;
+export const ACCOUNT_EMAIL_UPDATE_WINDOW: number = hourMilliseconds;
 export const ACCOUNT_DELETION_SUSPENSION_DURATION: number = dayMilliseconds;
