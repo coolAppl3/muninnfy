d89e396de5a6775289fc90246e9368ca1fd661ae
coolAppl3 <yanal.mohsen@yahoo.com>
2025-12-11 01:34:29 +0100

    fix: accountDeletionEmailTemplate() requiring a token instead of a confirmation code

--- src/util/email/emailTemplates/accountDeletionEmailTemplate.ts
+++ src/util/email/emailTemplates/accountDeletionEmailTemplate.ts
@@ -1,16 +1,9 @@
 export type AccountDeletionEmailTemplate = {
   displayName: string;
-  publicAccountId: string;
-  deletionToken: string;
+  confirmationCode: string;
 };
 
-export default function accountDeletionEmailTemplate({
-  displayName,
-  publicAccountId,
-  deletionToken,
-}: AccountDeletionEmailTemplate): string {
-  const origin: string = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://muninnfy.com';
-
+export default function accountDeletionEmailTemplate({ displayName, confirmationCode }: AccountDeletionEmailTemplate): string {
   return `
     <!DOCTYPE html>
     <html lang="en">
@@ -26,12 +19,7 @@ export default function accountDeletionEmailTemplate({
         <div style="background-color: #181b13; padding: 20px; color: #cbcbcd; font-family: sans-serif; font-weight: 500">
           <p>Hey ${displayName},</p>
 
-          <p>
-            We received a request to delete your account. To continue, please click the following link:
-            <a href="${origin}/sign-up/verification?publicAccountId=${publicAccountId}&deletionToken=${deletionToken}"
-              >${origin}/sign-up/verification?publicAccountId=${publicAccountId}&deletionToken=${deletionToken}</a
-            >.
-          </p>
+           <p>We received a request to delete your account. To confirm this action, please use the following confirmation code: ${confirmationCode}.</p>
 
           <p>
             If you didn't make this request, please sign in and change your password to ensure any unauthorized access is removed. Otherwise, we
