46e2f946eabf16c6b89adca9341a2cc40b7963b4
coolAppl3 <yanal.mohsen@yahoo.com>
2025-12-16 03:24:15 +0100

    change: temporarily include ongoing account requests data  into AccountDetailsContext

--- client/src/pages/Account/contexts/AccountDetailsContext.ts
+++ client/src/pages/Account/contexts/AccountDetailsContext.ts
@@ -1,10 +1,16 @@
 import { createContext, Dispatch, SetStateAction } from 'react';
-import { AccountDetailsType } from '../../../types/accountTypes';
+import { AccountDetailsType, OngoingAccountRequest } from '../../../types/accountTypes';
 
 export type AccountDetailsContextType = {
   accountDetails: AccountDetailsType;
   setAccountDetails: Dispatch<SetStateAction<AccountDetailsType>>;
+
+  ongoingEmailUpdateRequest: (OngoingAccountRequest & { new_email: string }) | null;
+  setOngoingEmailUpdateRequest: Dispatch<SetStateAction<(OngoingAccountRequest & { new_email: string }) | null>>;
+
+  ongoingAccountDeletionRequest: OngoingAccountRequest | null;
+  setOngoingAccountDeletionRequest: Dispatch<SetStateAction<OngoingAccountRequest | null>>;
 };
 
-const AccountDetailsContext = createContext<AccountDetailsContextType | null>(null);
-export default AccountDetailsContext;
+const AccountContext = createContext<AccountDetailsContextType | null>(null);
+export default AccountContext;
--- client/src/pages/Account/hooks/useAccountDetails.ts
+++ client/src/pages/Account/hooks/useAccountDetails.ts
@@ -1,8 +1,8 @@
 import { useContext } from 'react';
-import AccountDetailsContext, { AccountDetailsContextType } from '../contexts/AccountDetailsContext';
+import AccountContext, { AccountDetailsContextType } from '../contexts/AccountDetailsContext';
 
 export default function useAccountDetails(): AccountDetailsContextType {
-  const context = useContext<AccountDetailsContextType | null>(AccountDetailsContext);
+  const context = useContext<AccountDetailsContextType | null>(AccountContext);
 
   if (!context) {
     throw new Error('useAccountDetails must be used within AccountDetailsProvider.');
