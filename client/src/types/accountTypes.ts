e7eeeca8196790cf247f9d65fe32c3377bf576c2
coolAppl3 <yanal.mohsen@yahoo.com>
2025-12-16 03:23:04 +0100

    feat: add OngoingAccountRequest type

--- client/src/types/accountTypes.ts
+++ client/src/types/accountTypes.ts
@@ -7,3 +7,9 @@ export type AccountDetailsType = {
   is_private: boolean;
   approve_follow_requests: boolean;
 };
+
+export type OngoingAccountRequest = {
+  request_id: number;
+  is_suspended: boolean;
+  expiry_timestamp: number;
+};
