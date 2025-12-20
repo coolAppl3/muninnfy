fc03777afab3244b143f34ea695c283ec0c4306f
coolAppl3 <yanal.mohsen@yahoo.com>
2025-12-20 02:03:50 +0100

    feat: add AccountProfileUtils utility module with isValidOngoingRequestData()

---
+++ client/src/pages/Account/AccountProfile/util/AccountProfileUtils.ts
@@ -0,0 +1,21 @@
+import { OngoingAccountRequest } from '../../../../types/accountTypes';
+
+export function isValidOngoingRequestData(errResData: unknown): errResData is OngoingAccountRequest {
+  if (typeof errResData !== 'object' || errResData === null) {
+    return false;
+  }
+
+  if (
+    !('expiry_timestamp' in errResData) ||
+    typeof errResData.expiry_timestamp !== 'number' ||
+    !Number.isInteger(errResData.expiry_timestamp)
+  ) {
+    return false;
+  }
+
+  if (!('is_suspended' in errResData) || typeof errResData.is_suspended !== 'boolean') {
+    return false;
+  }
+
+  return true;
+}
