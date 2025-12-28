b6f770fe16b9fdcec347d403d30011344b09a738
coolAppl3 <yanal.mohsen@yahoo.com>
2025-12-28 02:33:19 +0100

    feat: add ContentLoadingSkeleton as a shared component under Account

---
+++ client/src/pages/Account/components/ContentLoadingSkeleton.tsx
@@ -0,0 +1,21 @@
+import { JSX } from 'react';
+
+export default function ContentLoadingSkeleton(): JSX.Element {
+  return (
+    <div className='grid gap-1'>
+      {Array.from({ length: 3 }).map((_, index: number) => (
+        <div
+          key={index}
+          className={`${skeletonContainerClassname} grid gap-1`}
+        >
+          <div className={`${skeletonTextClassname} h-2 max-w-3/4`}></div>
+          <div className={`${skeletonTextClassname} h-1 max-w-9/10`}></div>
+          <div className={`${skeletonTextClassname} h-1 max-w-1/4`}></div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+const skeletonTextClassname: string = 'w-full bg-cta/15 rounded-pill animate-pulse';
+const skeletonContainerClassname: string = 'bg-dark p-2 rounded-sm shadow-simple-tiny';
