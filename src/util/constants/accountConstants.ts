0b34a326f07d0d73fbba9dd6d45413da9320769a
coolAppl3 <yanal.mohsen@yahoo.com>
2025-12-31 20:31:56 +0100

    feat: add ACCOUNT_MAX_FOLLOWING_LIMIT constant

--- src/util/constants/accountConstants.ts
+++ src/util/constants/accountConstants.ts
@@ -12,3 +12,4 @@ export const ACCOUNT_EMAIL_UPDATE_WINDOW: number = hourMilliseconds;
 export const ACCOUNT_UPDATE_SUSPENSION_DURATION: number = dayMilliseconds;
 
 export const ACCOUNT_SOCIAL_FETCH_BATCH_SIZE: number = 100;
+export const ACCOUNT_MAX_FOLLOWING_LIMIT: number = 500;
