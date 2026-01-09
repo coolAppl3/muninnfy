117713e1a2fc78e108a7c23dd7088a0eee7c6aee
coolAppl3 <yanal.mohsen@yahoo.com>
2026-01-09 22:42:43 +0100

    feat: implement initial fetching and rendering logic

--- client/src/pages/Account/AccountSocial/components/AccountSocialFollowers/AccountSocialFollowers.tsx
+++ client/src/pages/Account/AccountSocial/components/AccountSocialFollowers/AccountSocialFollowers.tsx
@@ -1,22 +1,69 @@
-import { JSX } from 'react';
+import { JSX, useState } from 'react';
 import useAccountSocialDetails from '../../../hooks/useAccountSocialDetails';
 import FollowCard from '../FollowCard/FollowCard';
 import { FollowDetails } from '../../../../../types/socialTypes';
+import { getFollowersBatchService } from '../../../../../services/socialServices';
+import { SOCIAL_FETCH_BATCH_SIZE, SOCIAL_RENDER_BATCH_SIZE } from '../../../../../utils/constants/socialConstants';
+import usePopupMessage from '../../../../../hooks/usePopupMessage';
+import useHandleAsyncError, { HandleAsyncErrorFunction } from '../../../../../hooks/useHandleAsyncError';
 
 export default function AccountSocialFollowers(): JSX.Element {
-  const { followers } = useAccountSocialDetails();
+  const { followers, setFollowers, fetchDetails, setFetchDetails } = useAccountSocialDetails();
+
+  const [renderLimit, setRenderLimit] = useState<number>(SOCIAL_RENDER_BATCH_SIZE);
+  const allFollowersRendered: boolean = renderLimit >= followers.length && fetchDetails.allFollowersFetched;
+
+  const { displayPopupMessage } = usePopupMessage();
+  const handleAsyncError: HandleAsyncErrorFunction = useHandleAsyncError();
+
+  async function getFollowersBatch(): Promise<void> {
+    try {
+      const followersBatch: FollowDetails[] = (await getFollowersBatchService(0)).data.followersBatch;
+
+      setFollowers((prev) => [...prev, ...followersBatch]);
+      followersBatch.length < SOCIAL_FETCH_BATCH_SIZE && setFetchDetails((prev) => ({ ...prev, allFollowersFetched: true }));
+    } catch (err: unknown) {
+      console.log(err);
+      const { status } = handleAsyncError(err);
+
+      if (status === 400) {
+        displayPopupMessage('Something went wrong.', 'error');
+      }
+    }
+  }
 
   return (
     <>
       <div className='h-line mt-1 mb-2'></div>
       <div className='grid md:grid-cols-2 gap-1 items-start'>
-        {followers.map((followDetails: FollowDetails) => (
-          <FollowCard
-            key={followDetails.follow_id}
-            isFollowerCard={true}
-            followDetails={followDetails}
-          />
-        ))}
+        {followers.map((followDetails: FollowDetails, index: number) =>
+          index >= renderLimit ? (
+            <></>
+          ) : (
+            <FollowCard
+              key={followDetails.follow_id}
+              isFollowerCard={true}
+              followDetails={followDetails}
+            />
+          )
+        )}
+
+        {!allFollowersRendered && (
+          <button
+            type='button'
+            className='link text-sm max-w-fit mx-auto sm:col-span-2'
+            onClick={async () => {
+              if (allFollowersRendered) {
+                return;
+              }
+
+              renderLimit + SOCIAL_RENDER_BATCH_SIZE > followers.length && (await getFollowersBatch());
+              setRenderLimit((prev) => prev + SOCIAL_RENDER_BATCH_SIZE);
+            }}
+          >
+            Load more
+          </button>
+        )}
       </div>
     </>
   );
