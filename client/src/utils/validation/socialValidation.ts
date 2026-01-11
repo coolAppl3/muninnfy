fa9047d619ea3c3c47ef1e4f51f10069d41755da
coolAppl3 <yanal.mohsen@yahoo.com>
2026-01-11 20:49:36 +0100

    change: reduce restrictions on search query validation

--- client/src/utils/validation/socialValidation.ts
+++ client/src/utils/validation/socialValidation.ts
@@ -1,17 +1,19 @@
-import { containsInvalidWhitespace } from '../globalUtils';
-
 export function validateSearchQuery(value: string): string | null {
-  if (containsInvalidWhitespace(value)) {
-    return 'Search query must not contain consecutive whitespaces.';
+  if (value.length === 0) {
+    return null;
+  }
+
+  if (value.trim() === '') {
+    return 'Search query must include at least one valid character.';
   }
 
   if (value.length > 25) {
     return 'Search query must not exceed 25 characters.';
   }
 
-  const regex: RegExp = /^(?=.{1,25}$)(?!.*  )[A-Za-z0-9._]+(?: [A-Za-z0-9._]+)*$/;
+  const regex: RegExp = /^(?=.{1,25}$)(?=.*[A-Za-z0-9._])[A-Za-z0-9._\s]*$/;
   if (!regex.test(value)) {
-    return 'Only English letters, numbers, and the following symbols are allowed: `_`, `.`.';
+    return 'Only English letters, numbers, whitespace, and the following symbols are allowed: `_`, `.`.';
   }
 
   return null;
