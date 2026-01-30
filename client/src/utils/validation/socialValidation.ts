6445c640b08233d3936e499d04d4499118dba8bb
coolAppl3 <yanal.mohsen@yahoo.com>
2026-01-30 01:52:05 +0100

    feat: add validateSocialFindQuery()

--- client/src/utils/validation/socialValidation.ts
+++ client/src/utils/validation/socialValidation.ts
@@ -1,3 +1,5 @@
+import { isValidUuid } from './generalValidation';
+
 export function validateSearchQuery(value: string): string | null {
   if (value.length === 0) {
     return null;
@@ -12,6 +14,39 @@ export function validateSearchQuery(value: string): string | null {
   }
 
   const regex: RegExp = /^(?=.{1,25}$)(?=.*[A-Za-z0-9._])[A-Za-z0-9._\s]*$/;
+  if (!regex.test(value)) {
+    return 'Only English letters, numbers, whitespace, and the following symbols are allowed: `_`, `.`.';
+  }
+
+  return null;
+}
+
+export function validateSocialFindQuery(value: string): string | null {
+  if (value.length === 0) {
+    return 'A valid search query is required.';
+  }
+
+  if (/\s/.test(value)) {
+    return 'Search query must not contain any whitespace.';
+  }
+
+  if (value.includes('-')) {
+    if (isValidUuid(value)) {
+      return null;
+    }
+
+    return 'Invalid account ID.';
+  }
+
+  if (value.length < 3) {
+    return 'Search query must at least contain 3 characters.';
+  }
+
+  if (value.length > 25) {
+    return 'Username-based search query must not exceed 25 characters.';
+  }
+
+  const regex: RegExp = /^[A-Za-z0-9._]{3,25}$/;
   if (!regex.test(value)) {
     return 'Only English letters, numbers, whitespace, and the following symbols are allowed: `_`, `.`.';
   }
