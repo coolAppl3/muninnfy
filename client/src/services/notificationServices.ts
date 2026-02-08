fa7fc799bff90ff31fc417e1931b9fa8e47c4774
coolAppl3 <yanal.mohsen@yahoo.com>
2026-02-09 00:00:32 +0100

    feat: add notificationServices with getNotificationsBatchService()

---
+++ client/src/services/notificationServices.ts
@@ -0,0 +1,10 @@
+import axios, { AxiosResponse } from 'axios';
+import { NotificationDetails } from '../types/notificationTypes';
+
+axios.defaults.withCredentials = true;
+const notificationsApiUrl: string =
+  location.hostname === 'localhost' ? `http://localhost:5000/api/notifications` : `https://muninnfy/api/notifications`;
+
+async function getNotificationsBatchService(offset: number): Promise<AxiosResponse<NotificationDetails[]>> {
+  return axios.get(`${notificationsApiUrl}/${offset}`);
+}
