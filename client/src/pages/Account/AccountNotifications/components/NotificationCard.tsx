a60545fddea871d9266eed7e2aec0bd301a2f391
coolAppl3 <yanal.mohsen@yahoo.com>
2026-02-08 20:48:35 +0100

    feat: add NotificationCard page component

---
+++ client/src/pages/Account/AccountNotifications/components/NotificationCard.tsx
@@ -0,0 +1,38 @@
+import { JSX } from 'react';
+import { NotificationDetails, NotificationType } from '../../../../types/notificationTypes';
+import { getDateAndTimeString } from '../../../../utils/globalUtils';
+
+type NotificationCardProps = {
+  notification: NotificationDetails;
+};
+
+export default function NotificationCard({ notification }: NotificationCardProps): JSX.Element {
+  const { notification_type, notification_timestamp, sender_public_account_id, sender_username, sender_display_name } = notification;
+
+  const notificationDescription: string = getNotificationDescription(notification_type);
+
+  return (
+    <div className='p-1 bg-primary rounded text-description text-sm'>
+      <p className='text-title'>
+        {sender_display_name} (@{sender_username}) {notificationDescription}
+      </p>
+
+      <div className='text-xs text-description/50 mt-1'>
+        <p>{getDateAndTimeString(notification_timestamp, true)}</p>
+        <p>{sender_public_account_id}</p>
+      </div>
+    </div>
+  );
+}
+
+function getNotificationDescription(notificationType: NotificationType): string {
+  if (notificationType === 'NEW_FOLLOWER') {
+    return 'started following you.';
+  }
+
+  if (notificationType === 'NEW_FOLLOW_REQUEST') {
+    return 'requested to follow you.';
+  }
+
+  return 'accepted your follow request.';
+}
