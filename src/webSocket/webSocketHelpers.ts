1486eaa6603205048c8c14825f0e94d961ce4234
coolAppl3 <yanal.mohsen@yahoo.com>
2026-02-08 01:47:47 +0100

    change: further rework sendWebSocketMessage(), renaming it to sendWebSocketNotification(), and tightening the behavior to align with what type of data will be sent

--- src/webSocket/webSocketHelpers.ts
+++ src/webSocket/webSocketHelpers.ts
@@ -2,13 +2,18 @@ import { RowDataPacket } from 'mysql2/promise';
 import { dbPool } from '../db/db';
 import { WebSocketDetails, wsMap } from './webSocketServer';
 
-type WebSocketMessage = {
-  type: string;
-  reason: string;
-  data: { [key: string]: unknown };
+export type NotificationType = 'NEW_FOLLOWER' | 'NEW_FOLLOW_REQUEST' | 'FOLLOW_REQUEST_ACCEPTED';
+
+export type NotificationDetails = {
+  notification_id: number;
+  sender_public_account_id: string;
+  sender_username: string;
+  sender_display_name: string;
+  notification_timestamp: number;
+  notification_type: NotificationType;
 };
 
-export async function sendWebSocketMessage(accountId: number, data: WebSocketMessage): Promise<void> {
+export async function sendWebSocketNotification(accountId: number, notificationDetails: NotificationDetails): Promise<void> {
   try {
     type AuthSessionDetails = {
       authSessionId: string;
@@ -31,7 +36,7 @@ export async function sendWebSocketMessage(accountId: number, data: WebSocketMes
         return;
       }
 
-      wsDetails.ws.send(JSON.stringify(data), (err: Error | undefined) => err && console.log(err));
+      wsDetails.ws.send(JSON.stringify(notificationDetails), (err: Error | undefined) => err && console.log(err));
     }
   } catch (err: unknown) {
     console.log(err);
