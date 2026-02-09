e6ee078d818a5e83d507ffc27e97d1ae9813ba89
coolAppl3 <yanal.mohsen@yahoo.com>
2026-02-09 02:34:49 +0100

    feat: add abortSignal to getNotificationsBatchService()

--- client/src/services/notificationServices.ts
+++ client/src/services/notificationServices.ts
@@ -5,6 +5,9 @@ axios.defaults.withCredentials = true;
 const notificationsApiUrl: string =
   location.hostname === 'localhost' ? `http://localhost:5000/api/notifications` : `https://muninnfy/api/notifications`;
 
-export async function getNotificationsBatchService(offset: number): Promise<AxiosResponse<NotificationDetails[]>> {
-  return axios.get(`${notificationsApiUrl}/${offset}`);
+export async function getNotificationsBatchService(
+  offset: number,
+  abortSignal: AbortSignal
+): Promise<AxiosResponse<NotificationDetails[]>> {
+  return axios.get(`${notificationsApiUrl}/${offset}`, { signal: abortSignal });
 }
