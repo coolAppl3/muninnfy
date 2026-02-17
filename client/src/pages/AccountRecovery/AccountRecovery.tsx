731a614fed70faac3d01c93983e4459bceaf7688
coolAppl3 <yanal.mohsen@yahoo.com>
2026-02-17 01:49:20 +0100

    feat: implement ResendAccountRecoveryEmail and InvalidRecoveryLink

--- client/src/pages/AccountRecovery/AccountRecovery.tsx
+++ client/src/pages/AccountRecovery/AccountRecovery.tsx
@@ -1,16 +1,22 @@
-import { JSX } from 'react';
+import { Dispatch, JSX, SetStateAction, useState } from 'react';
 import Head from '../../components/Head/Head';
 import Container from '../../components/Container/Container';
 import StartAccountRecovery from './components/StartAccountRecovery';
 import ResendAccountRecoveryEmail from './components/ResendAccountRecoveryEmail';
 import { useSearchParams } from 'react-router-dom';
 import ConfirmAccountRecovery from './components/ConfirmAccountRecovery';
+import { isValidUuid } from '../../utils/validation/generalValidation';
+import { InvalidRecoveryLink } from './components/InvalidRecoveryLink';
 
 export default function AccountRecovery(): JSX.Element {
   const [urlSearchParams] = useSearchParams();
 
   const publicAccountId: string | null = urlSearchParams.get('publicAccountId');
   const recoveryToken: string | null = urlSearchParams.get('recoveryToken');
+
+  const [isValidRecoveryLink, setIsValidRecoveryLink] = useState<boolean>(
+    (publicAccountId ? isValidUuid(publicAccountId) : true) && (recoveryToken ? isValidUuid(recoveryToken) : true)
+  );
 
   return (
     <>
@@ -20,6 +26,8 @@ export default function AccountRecovery(): JSX.Element {
         <Container>
           <div className='py-3 px-2 bg-secondary rounded-sm shadow-simple max-w-[36rem] mx-auto'>
             <AccountRecoveryChildren
+              linkParamsValid={isValidRecoveryLink}
+              setIsValidRecoveryLink={setIsValidRecoveryLink}
               publicAccountId={publicAccountId}
               recoveryToken={recoveryToken}
             />
@@ -31,17 +39,33 @@ export default function AccountRecovery(): JSX.Element {
 }
 
 type AccountRecoveryChildrenProps = {
+  linkParamsValid: boolean;
+  setIsValidRecoveryLink: Dispatch<SetStateAction<boolean>>;
   publicAccountId: string | null;
   recoveryToken: string | null;
 };
 
-function AccountRecoveryChildren({ publicAccountId, recoveryToken }: AccountRecoveryChildrenProps): JSX.Element {
+function AccountRecoveryChildren({
+  linkParamsValid,
+  setIsValidRecoveryLink,
+  publicAccountId,
+  recoveryToken,
+}: AccountRecoveryChildrenProps): JSX.Element {
+  if (!linkParamsValid) {
+    return <InvalidRecoveryLink />;
+  }
+
   if (!publicAccountId) {
     return <StartAccountRecovery />;
   }
 
   if (!recoveryToken) {
-    return <ResendAccountRecoveryEmail />;
+    return (
+      <ResendAccountRecoveryEmail
+        publicAccountId={publicAccountId}
+        setIsValidRecoveryLink={setIsValidRecoveryLink}
+      />
+    );
   }
 
   return <ConfirmAccountRecovery />;
