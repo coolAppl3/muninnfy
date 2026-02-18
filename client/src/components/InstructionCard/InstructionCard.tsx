f19aea79dd4375bde70c55e1aaa0a70a0328e00f
coolAppl3 <yanal.mohsen@yahoo.com>
2026-02-19 00:52:40 +0100

    feat: add InstructionCard utility component

---
+++ client/src/components/InstructionCard/InstructionCard.tsx
@@ -0,0 +1,27 @@
+import { JSX } from 'react';
+import Button from '../Button/Button';
+
+type InstructionCardProps = {
+  title: string;
+  description: string;
+  btnTitle: string;
+  onClick: () => void;
+  btnDisabled: boolean;
+};
+
+export default function InstructionCard({ title, description, btnTitle, onClick, btnDisabled = false }: InstructionCardProps): JSX.Element {
+  return (
+    <>
+      <h4 className='text-title font-medium mb-1'>{title}</h4>
+      <p className='text-description text-sm mb-2'>{description}</p>
+
+      <Button
+        className='bg-description border-description text-dark w-full'
+        disabled={btnDisabled}
+        onClick={onClick}
+      >
+        {btnTitle}
+      </Button>
+    </>
+  );
+}
