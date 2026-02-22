4e81f69256f7f56b7b671c172f17330bb6cc8a94
coolAppl3 <yanal.mohsen@yahoo.com>
2026-02-23 00:10:56 +0100

    fix: type name not aligning with checkForAuthSessionService()

--- client/src/services/authServices.ts
+++ client/src/services/authServices.ts
@@ -3,11 +3,11 @@ import axios, { AxiosResponse } from 'axios';
 axios.defaults.withCredentials = true;
 const authApiUrl: string = location.hostname === 'localhost' ? `http://localhost:5000/api/auth` : `https://muninnfy/api/auth`;
 
-type GetAuthSessionServiceData = {
+type CheckForAuthSessionServiceData = {
   isValidAuthSession: boolean;
 };
 
-export function checkForAuthSessionService(abortSignal: AbortSignal): Promise<AxiosResponse<GetAuthSessionServiceData>> {
+export function checkForAuthSessionService(abortSignal: AbortSignal): Promise<AxiosResponse<CheckForAuthSessionServiceData>> {
   return axios.get(`${authApiUrl}/session`, { signal: abortSignal });
 }
 
