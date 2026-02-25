7b605d9dda00a22b6dac431ad11c71fc73b3c7ca
coolAppl3 <yanal.mohsen@yahoo.com>
2026-02-25 03:20:49 +0100

    refactor: remove non-null assertion

--- src/util/email/initTransporter.ts
+++ src/util/email/initTransporter.ts
@@ -3,7 +3,7 @@ import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 export const emailTransporter = nodemailer.createTransport({
   host: process.env.TRANSPORTER_HOST,
-  port: +process.env.TRANSPORTER_PORT!,
+  port: +(process.env.TRANSPORTER_PORT || 465),
   secure: true,
   auth: {
     user: process.env.TRANSPORTER_USER,
