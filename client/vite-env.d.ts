e941058b65843ff22b9bf565aaf43bac10d99992
coolAppl3 <yanal.mohsen@icloud.com>
2026-03-15 02:16:43 +0100

    feat: add ImportMeta and ImportMetaEnv

--- client/vite-env.d.ts
+++ client/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  readonly MODE: string;
+  readonly VITE_API_DEV_URL: string;
+  readonly VITE_API_PROD_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
