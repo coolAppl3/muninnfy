fae8ffadf2610ee6f10feb6fa69c068d394c115a
coolAppl3 <yanal.mohsen@icloud.com>
2026-03-15 03:10:40 +0100

    refactor: use environment variables

--- client/vite-env.d.ts
+++ client/vite-env.d.ts
@@ -2,8 +2,12 @@
 
 interface ImportMetaEnv {
   readonly MODE: string;
+
   readonly VITE_API_DEV_URL: string;
   readonly VITE_API_PROD_URL: string;
+
+  readonly VITE_WS_DEV_URL: string;
+  readonly VITE_WS_PROD_URL: string;
 }
 
 interface ImportMeta {
