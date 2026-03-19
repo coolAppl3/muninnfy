6e6ee2d94737c58e07f673fa64010b3517f0d77e
coolAppl3 <yanal.mohsen@icloud.com>
2026-03-19 02:34:55 +0100

    feat: add ViewAccountProfile component and partially implement it

---
+++ client/src/pages/ViewAccount/components/ViewAccountProfile/ViewAccountProfile.tsx
@@ -0,0 +1,89 @@
+import { JSX } from 'react';
+import StatisticItem from '../../../../components/StatisticItem/StatisticItem';
+import { ViewAccountDetailsType } from '../../../../types/accountTypes';
+import { getFullDateString } from '../../../../utils/globalUtils';
+import useAccountLocation from '../../../Account/hooks/useAccountLocation';
+import useAccountSocial from '../../../Account/hooks/useAccountSocial';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
+import Button from '../../../../components/Button/Button';
+
+type ViewAccountProfileProps = {
+  viewAccountDetails: ViewAccountDetailsType;
+};
+
+export default function ViewAccountProfile({ viewAccountDetails }: ViewAccountProfileProps): JSX.Element {
+  const { public_account_id, username, display_name, created_on_timestamp, is_following } = viewAccountDetails;
+
+  const { setAccountLocation } = useAccountLocation();
+  const { setSocialSection } = useAccountSocial();
+  const navigate: NavigateFunction = useNavigate();
+
+  return (
+    <section className='p-2 bg-secondary rounded-sm col-span-12 md:col-span-9 grid gap-[1.6rem] shadow-simple-tiny md:min-h-full'>
+      <div className=''>
+        <h3 className='text-title font-normal mb-[0px]'>{display_name}</h3>
+        <h3 className='text-description text-sm font-normal'>@{username}</h3>
+      </div>
+
+      <div className='grid grid-cols-3 gap-1 text-sm text-description relative z-0 h-fit '>
+        <button
+          className='w-fit text-start cursor-pointer hover:text-cta'
+          onClick={() => navigate(`/view/wishlists/${public_account_id}`)}
+        >
+          <StatisticItem
+            title='Wishlists'
+            value={8}
+          />
+        </button>
+
+        <button
+          className='w-fit text-start cursor-pointer hover:text-cta'
+          onClick={() => setAccountLocation('social')}
+        >
+          <StatisticItem
+            title='Followers'
+            value={32}
+          />
+        </button>
+
+        <button
+          className='w-fit text-start cursor-pointer hover:text-cta'
+          onClick={() => {
+            setAccountLocation('social');
+            setSocialSection('following');
+          }}
+        >
+          <StatisticItem
+            title='Following'
+            value={41}
+          />
+        </button>
+      </div>
+
+      <div className='text-description/50 text-xs'>
+        <p className='leading-none mb-[4px]'>Created on {getFullDateString(created_on_timestamp)}</p>
+        <p className='leading-none'>{public_account_id}</p>
+      </div>
+
+      {is_following ? (
+        <Button
+          className='bg-secondary border-title text-title w-full sm:w-fit'
+          onClick={() => {
+            /** TODO: continue implementation */
+          }}
+        >
+          Unfollow
+        </Button>
+      ) : (
+        <Button
+          className='bg-cta border-cta text-dark w-full sm:w-fit'
+          onClick={() => {
+            /** TODO: continue implementation */
+          }}
+        >
+          Follow
+        </Button>
+      )}
+    </section>
+  );
+}
