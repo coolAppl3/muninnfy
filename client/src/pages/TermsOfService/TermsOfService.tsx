45390a01a28146bd64a4d947e0b7b75efd54a233
Yanal Mohsen <yanal.mohsen@icloud.com>
2026-04-06 22:49:06 +0200

    fix: add missing Head component

--- client/src/pages/TermsOfService/TermsOfService.tsx
+++ client/src/pages/TermsOfService/TermsOfService.tsx
@@ -1,15 +1,20 @@
 import { JSX } from 'react';
 import Container from '../../components/Container/Container';
 import TermsOfServiceMdx from './TermsOfService.mdx';
+import Head from '../../components/Head/Head';
 
 export default function TermsOfService(): JSX.Element {
   return (
-    <main className='py-4'>
-      <Container>
-        <article className='max-w-[700px] text-description'>
-          <TermsOfServiceMdx />
-        </article>
-      </Container>
-    </main>
+    <>
+      <Head title='Terms of Service - Muninnfy' />
+
+      <main className='py-4'>
+        <Container>
+          <article className='max-w-[700px] text-description'>
+            <TermsOfServiceMdx />
+          </article>
+        </Container>
+      </main>
+    </>
   );
 }
