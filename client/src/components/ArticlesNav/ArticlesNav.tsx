42657bcda2625788676249e6e9444cb453dce51a
Yanal Mohsen <yanal.mohsen@icloud.com>
2026-04-09 02:43:32 +0200

    feat: simplify styling

--- client/src/components/ArticlesNav/ArticlesNav.tsx
+++ client/src/components/ArticlesNav/ArticlesNav.tsx
@@ -2,33 +2,28 @@ import { JSX } from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default function ArticlesNav(): JSX.Element {
-  const styling: string = 'block p-2 bg-secondary transition-[filter] hover:brightness-75';
+  const styling: string =
+    'block p-2 bg-secondary transition-[filter] hover:brightness-75 border-b-1 border-b-light-gray last:border-b-secondary';
 
   return (
     <nav className='hidden md:block col-span-4 rounded shadow-simple-tiny font-medium text-title sticky top-10'>
       <NavLink
         to={'/terms-of-service'}
-        className={({ isActive }) =>
-          `${styling} border-b-1 border-b-light-gray ${isActive ? 'text-cta' : ''}`
-        }
+        className={({ isActive }) => `${styling} ${isActive ? 'text-cta' : ''}`}
       >
         Terms of Service
       </NavLink>
 
       <NavLink
         to={'/privacy-policy'}
-        className={({ isActive }) =>
-          `${styling} border-b-1 border-b-light-gray ${isActive ? 'text-cta' : ''}`
-        }
+        className={({ isActive }) => `${styling} ${isActive ? 'text-cta' : ''}`}
       >
         Privacy Policy
       </NavLink>
 
       <NavLink
         to={'/cookie-policy'}
-        className={({ isActive }) =>
-          `${styling} border-b-1 border-b-light-gray ${isActive ? 'text-cta' : ''}`
-        }
+        className={({ isActive }) => `${styling} ${isActive ? 'text-cta' : ''}`}
       >
         Cookie Policy
       </NavLink>
