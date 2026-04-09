f84f7458e6c30af0ef2b71b7275727181c7167e7
Yanal Mohsen <yanal.mohsen@icloud.com>
2026-04-09 02:43:25 +0200

    improvement: improve visual UI design

--- client/src/pages/Faq/components/AccordionItem.tsx
+++ client/src/pages/Faq/components/AccordionItem.tsx
@@ -14,7 +14,7 @@ export default function AccordionItem({ question, answer }: AccordionItemProps):
     <div className='bg-secondary border-b-1 last:border-b-secondary not-last:border-b-light-gray'>
       <button
         type='button'
-        className={`w-full flex justify-between items-start p-2 transition-colors text-title hover:text-cta cursor-pointer border-b-1 ${isExpanded ? 'border-b-light-gray' : 'border-b-secondary'}`}
+        className='w-full flex justify-between items-start p-2 transition-colors text-title hover:text-cta cursor-pointer'
         onClick={() => setIsExpanded((prev) => !prev)}
       >
         <span className='text-start font-medium'>{question}</span>
@@ -30,6 +30,8 @@ export default function AccordionItem({ question, answer }: AccordionItemProps):
       <div
         className={`overflow-hidden transform-gpu transition-all duration-300 ${isExpanded ? 'max-h-[300px]' : 'max-h-0'}`}
       >
+        <div className='h-line h-[1px] w-8/10 mr-auto'></div>
+
         <p className='px-2 py-2 text-description text-sm whitespace-break-spaces'>{answer}</p>
       </div>
     </div>
