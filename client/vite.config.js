41719197a28d02d3ce25e03b437d3f7513888654
Yanal Mohsen <yanal.mohsen@icloud.com>
2026-04-10 02:20:06 +0200

    build: add rollup-plugin-visualizer

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 import mdx from '@mdx-js/rollup';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': '"production"',
+  },
+
   plugins: [
     {
       enforce: 'pre',
@@ -20,7 +25,14 @@ export default defineConfig({
         },
       },
     }),
+
+    visualizer({
+      open: true, // automatically opens the report
+      gzipSize: true, // shows gzip sizes
+      brotliSize: true, // shows brotli sizes
+    }),
   ],
+
   server: {
     port: 3000,
   },
