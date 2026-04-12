bdf4a45bd93fca263c4763ed3efe4316e4d1debb
Yanal Mohsen <yanal.mohsen@icloud.com>
2026-04-13 01:15:23 +0200

    test: add test suite for Container component

---
+++ client/src/components/Container/Container.test.tsx
@@ -0,0 +1,23 @@
+import { describe, it, expect, vi } from 'vitest';
+import { render } from 'vitest-browser-react';
+import Container from './Container';
+
+describe('Container', () => {
+  it('should render a react node', async () => {
+    const { getByText } = await render(
+      <Container>
+        <span>someText</span>
+      </Container>
+    );
+
+    const span = getByText('someText');
+    await expect.element(span).toBeInTheDocument();
+  });
+
+  it('includes custom class names', async () => {
+    const { getByText } = await render(<Container className='someClass'>someText</Container>);
+
+    const container = getByText('someText');
+    await expect.element(container).toHaveClass('someClass');
+  });
+});
