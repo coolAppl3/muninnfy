68dc122456403818e33f4082544f18051606b274
Yanal Mohsen <yanal.mohsen@icloud.com>
2026-04-19 19:38:54 +0200

    refactor: component to improve readability

--- client/src/components/TimeWindowContainer/TimeWindowContainer.tsx
+++ client/src/components/TimeWindowContainer/TimeWindowContainer.tsx
@@ -1,8 +1,7 @@
-import { Dispatch, JSX, SetStateAction } from 'react';
+import { Dispatch, JSX, MouseEventHandler, SetStateAction } from 'react';
 import useCalendar from '../../hooks/useCalendar';
 import { getFullDateString } from '../../utils/globalUtils';
 import CrossIcon from '../../assets/svg/CrossIcon.svg?react';
-import { CalendarMode } from '../../contexts/CalendarContext';
 
 type TimeWindowContainerProps = {
   calendarKey: string;
@@ -28,10 +27,9 @@ export default function TimeWindowContainer({
   const startTimestamp: number | undefined = startTimestampsMap.get(calendarKey);
   const endTimestamp: number | undefined = endTimestampsMap.get(calendarKey);
 
-  function removeTimestamp(calendarMode: CalendarMode): void {
-    const stateSetter: Dispatch<SetStateAction<Map<string, number>>> =
-      calendarMode === 'start' ? setStartTimestampsMap : setEndTimestampsMap;
-
+  function handleRemoveTimestamp(
+    stateSetter: Dispatch<SetStateAction<Map<string, number>>>
+  ): void {
     stateSetter((prev) => {
       const nextMap = new Map<string, number>(prev);
       nextMap.delete(calendarKey);
@@ -42,46 +40,67 @@ export default function TimeWindowContainer({
 
   return (
     <div className={`grid gap-1 sm:gap-2 sm:grid-cols-2 ${className || ''}`}>
-      {Array.from({ length: 2 }, (_, index: number) => (
-        <div
-          key={index}
-          className='relative flex flex-col justify-center items-start gap-[6px]'
+      <div className='relative flex flex-col justify-center items-start gap-[6px]'>
+        <label
+          htmlFor='time-window-start'
+          className='text-sm font-medium text-title'
         >
-          <label
-            htmlFor={`time-window-${index === 0 ? 'start' : 'end'}`}
-            className='text-sm font-medium text-title'
-          >
-            {index === 0 ? startLabel : endLabel}
-          </label>
+          {startLabel}
+        </label>
 
-          <button
-            type='button'
-            id={`time-window-${index === 0 ? 'start' : 'end'}`}
-            onClick={() =>
-              index === 0
-                ? displayCalendar('start', calendarKey)
-                : displayCalendar('end', calendarKey)
-            }
-            className='w-full h-4 p-1 rounded border-1 border-description/75 hover:border-cta outline-0 text-description text-start text-sm transition-colors cursor-pointer'
-          >
-            {index === 0
-              ? startTimestamp && getFullDateString(startTimestamp)
-              : endTimestamp && getFullDateString(endTimestamp)}
-          </button>
+        <button
+          type='button'
+          id='time-window-start'
+          onClick={() => displayCalendar('start', calendarKey)}
+          className='w-full h-4 p-1 rounded border-1 border-description/75 hover:border-cta outline-0 text-description text-start text-sm transition-colors cursor-pointer'
+        >
+          {startTimestamp && getFullDateString(startTimestamp)}
+        </button>
 
-          {((index === 0 && startTimestamp) || (index === 1 && endTimestamp)) && (
-            <button
-              type='button'
-              className='absolute bottom-0 right-0 h-4 w-4 grid place-items-center cursor-pointer text-title transition-colors hover:text-cta'
-              title='Remove date'
-              aria-label='Remove date'
-              onClick={() => removeTimestamp(index === 0 ? 'start' : 'end')}
-            >
-              <CrossIcon className='w-[1.4rem] h-[1.4rem] rotate-45' />
-            </button>
-          )}
-        </div>
-      ))}
+        {startTimestamp && (
+          <RemoveTimestampBtn onClick={() => handleRemoveTimestamp(setStartTimestampsMap)} />
+        )}
+      </div>
+
+      <div className='relative flex flex-col justify-center items-start gap-[6px]'>
+        <label
+          htmlFor='time-window-end'
+          className='text-sm font-medium text-title'
+        >
+          {endLabel}
+        </label>
+
+        <button
+          type='button'
+          id='time-window-end'
+          onClick={() => displayCalendar('end', calendarKey)}
+          className='w-full h-4 p-1 rounded border-1 border-description/75 hover:border-cta outline-0 text-description text-start text-sm transition-colors cursor-pointer'
+        >
+          {endTimestamp && getFullDateString(endTimestamp)}
+        </button>
+
+        {endTimestamp && (
+          <RemoveTimestampBtn onClick={() => handleRemoveTimestamp(setEndTimestampsMap)} />
+        )}
+      </div>
     </div>
+  );
+}
+
+type RemoveTimestampBtnProps = {
+  onClick: MouseEventHandler<HTMLButtonElement>;
+};
+
+function RemoveTimestampBtn({ onClick }: RemoveTimestampBtnProps): JSX.Element {
+  return (
+    <button
+      type='button'
+      className='absolute bottom-0 right-0 h-4 w-4 grid place-items-center cursor-pointer text-title transition-colors hover:text-cta'
+      title='Remove date'
+      aria-label='Remove date'
+      onClick={onClick}
+    >
+      <CrossIcon className='w-[1.4rem] h-[1.4rem] rotate-45' />
+    </button>
   );
 }
