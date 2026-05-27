778f248b8d4977d698b33404e7a2825f0aa79747
Yanal Mohsen <yanal.mohsen@icloud.com>
2026-05-28 00:51:04 +0200

    feat: add setup file for backend testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,6 @@ export default defineConfig({
     environment: 'node',
     include: ['src/**/*.test.ts'],
     exclude: ['client/**', 'dist/**'],
+    setupFiles: ['./src/tests/setup.ts'],
   },
 });
