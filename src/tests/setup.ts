d053c690586a620be93e4862e976ecf4f30d111a
Yanal Mohsen <yanal.mohsen@icloud.com>
2026-05-30 03:17:48 +0200

    test: add global db mocks

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,5 +1,21 @@
 import { beforeEach, vi } from 'vitest';
 
+vi.mock('../db/db', () => ({
+  dbPool: {
+    getConnection: vi.fn().mockResolvedValue(mockConnection),
+    execute: vi.fn(),
+    query: vi.fn(),
+  },
+}));
+
+export const mockConnection = {
+  beginTransaction: vi.fn(),
+  execute: vi.fn(),
+  commit: vi.fn(),
+  rollback: vi.fn(),
+  release: vi.fn(),
+};
+
 beforeEach(() => {
   vi.clearAllMocks();
 });
