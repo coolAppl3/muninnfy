8445eba812e0548d8f53847470f523242bd0066f
Yanal Mohsen <yanal.mohsen@icloud.com>
2026-07-05 12:20:49 +0200

    improvement: rework POST wishlists to use a transaction to ensure the wishlists limit isn't exceeded

--- src/routers/wishlistsRouter.ts
+++ src/routers/wishlistsRouter.ts
@@ -5,7 +5,7 @@ import {
   isValidWishlistPrivacyLevel,
   isValidWishlistTitle,
 } from '../util/validation/wishlistValidation';
-import { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
+import { PoolConnection, ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 import { getAccountIdByAuthSessionId } from '../db/helpers/authDbHelpers';
 import { logUnexpectedError } from '../logs/errorLogger';
 import { dbPool } from '../db/db';
@@ -65,21 +65,25 @@ wishlistsRouter.post('/', async (req: Request, res: Response) => {
     return;
   }
 
+  let connection: PoolConnection | null = null;
+
   try {
+    connection = await dbPool.getConnection();
+    await connection.beginTransaction();
+
     type AccountWishlistsDetails = {
       wishlists_created_count: number;
-      title_already_used: boolean;
     };
 
-    const [accountWishlistsRows] = await dbPool.execute<RowDataPacket[]>(
+    const [accountWishlistsRows] = await connection.execute<RowDataPacket[]>(
       `SELECT
-        COUNT(*) AS wishlists_created_count,
-        EXISTS (SELECT 1 FROM wishlists WHERE title = :title AND account_id = :accountId ) AS title_already_used
+        COUNT(*) AS wishlists_created_count
       FROM
         wishlists
       WHERE
-        account_id = :accountId;`,
-      { accountId, title: title }
+        account_id = :accountId
+      FOR UPDATE;`,
+      { accountId }
     );
 
     const accountWishlistsDetails = accountWishlistsRows[0] as
@@ -87,31 +91,26 @@ wishlistsRouter.post('/', async (req: Request, res: Response) => {
       | undefined;
 
     if (!accountWishlistsDetails) {
+      await connection.rollback();
       res.status(500).json({ message: 'Internal server error.' });
-      await logUnexpectedError(req, null, 'Failed to fetch wishlists_created_count.');
 
+      await logUnexpectedError(req, null, 'Failed to fetch wishlists_created_count.');
       return;
     }
 
     if (accountWishlistsDetails.wishlists_created_count >= TOTAL_WISHLISTS_LIMIT) {
+      await connection.rollback();
       res
         .status(403)
         .json({ message: 'Wishlists limit reached.', reason: 'wishlistLimitReached' });
-      return;
-    }
 
-    if (accountWishlistsDetails.title_already_used) {
-      res.status(409).json({
-        message: 'You already have a wishlist with this title.',
-        reason: 'duplicateTitle',
-      });
       return;
     }
 
     const wishlistId: string = generateCryptoUuid();
     const currentTimestamp: number = Date.now();
 
-    await dbPool.execute(
+    await connection.execute(
       `INSERT INTO wishlists (
         wishlist_id,
         account_id,
@@ -134,9 +133,11 @@ wishlistsRouter.post('/', async (req: Request, res: Response) => {
       ]
     );
 
+    await connection.commit();
     res.status(201).json({ wishlistId });
   } catch (err: unknown) {
     console.log(err);
+    await connection?.rollback();
 
     if (res.headersSent) {
       await logUnexpectedError(req, err, 'Attempted to send two responses.');
@@ -160,6 +161,8 @@ wishlistsRouter.post('/', async (req: Request, res: Response) => {
 
     res.status(500).json({ message: 'Internal server error.' });
     await logUnexpectedError(req, err);
+  } finally {
+    connection?.release();
   }
 });
 
